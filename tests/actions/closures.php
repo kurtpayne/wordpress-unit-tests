c4dbf96035a308cd64aabd6b6644d5db83c644a5
Kurt Payne <kpayne@gmail.com>
2012-07-18 13:05:47 -0700

    Updating from SVN

--- tests/actions/closures.php
+++ tests/actions/closures.php
@@ -5,13 +5,13 @@
  *
  * @group hooks
  */
-class WP_Test_Actions_Closures extends WP_UnitTestCase {
+class Tests_Actions_Closures extends WP_UnitTestCase {
 
 	/**
 	 * @ticket 10493
 	 */
 	function test_action_closure() {
-		$tag = rand_str();
+		$tag = 'test_action_closure';
 		$closure = function($a, $b) { $GLOBALS[$a] = $b;};
 		add_action($tag, $closure, 10, 2);
 
@@ -22,7 +22,7 @@ class WP_Test_Actions_Closures extends WP_UnitTestCase {
 
 		$this->assertSame($GLOBALS[$context[0]], $context[1]);
 
-		$tag2 = rand_str();
+		$tag2 = 'test_action_closure_2';
 		$closure2 = function() { $GLOBALS['closure_no_args'] = true;};
 		add_action($tag2, $closure2);
 
@@ -31,5 +31,8 @@ class WP_Test_Actions_Closures extends WP_UnitTestCase {
 		do_action($tag2);
 
 		$this->assertTrue($GLOBALS['closure_no_args']);
+
+		remove_action( $tag, $closure );
+		remove_action( $tag2, $closure2 );
 	}
 }
