e479c6abae1f20c02d1705ee35b4cc5581fdaf31
Kurt Payne <kpayne@gmail.com>
2012-07-26 16:05:46 -0700

    Updating from SVN

--- tests/xmlrpc/wp/uploadFile.php
+++ tests/xmlrpc/wp/uploadFile.php
@@ -4,16 +4,17 @@
  * @group xmlrpc
  */
 class Tests_XMLRPC_wp_uploadFile extends WP_XMLRPC_UnitTestCase {
+
 	function test_valid_attachment() {
 		$this->make_user_by_role( 'editor' );
 
 		// create attachment
-		$filename = ( DIR_TESTDATA.'/images/a2-small.jpg' );
+		$filename = ( DIR_TESTDATA . '/images/a2-small.jpg' );
 		$contents = file_get_contents( $filename );
 		$data = array(
 			'name' => 'a2-small.jpg',
 			'type' => 'image/jpeg',
-			'bits' => base64_encode( $contents )
+			'bits' => $contents
 		);
 
 
@@ -29,7 +30,7 @@ class Tests_XMLRPC_wp_uploadFile extends WP_XMLRPC_UnitTestCase {
 	}
 
 	/**
-	 * @ticket 21292
+	 * @tickets 21292
 	 */
 	function test_network_limit() {
 		$this->make_user_by_role( 'editor' );
@@ -37,23 +38,42 @@ class Tests_XMLRPC_wp_uploadFile extends WP_XMLRPC_UnitTestCase {
 		update_option( 'blog_upload_space', 0.1 );
 
 		// create attachment
-		$filename = ( DIR_TESTDATA.'/images/canola.jpg' );
+		$filename = ( DIR_TESTDATA . '/images/canola.jpg' );
 		$contents = file_get_contents( $filename );
 		$data = array(
-			'name' => 'a2-small.jpg',
+			'name' => 'canola.jpg',
 			'type' => 'image/jpeg',
-			'bits' => base64_encode( $contents )
+			'bits' => $contents
 		);
 
 		$result = $this->myxmlrpcserver->mw_newMediaObject( array( 0, 'editor', 'editor', $data ) );
 
 		// Only multisite should have a limit
-		if ( is_multisite() ) {
+		if ( is_multisite() )
 			$this->assertInstanceOf( 'IXR_Error', $result );
-		}
-		else {
+		else
 			$this->assertNotInstanceOf( 'IXR_Error', $result );
-		}
+	}
 
+	/**
+	 * @ticket 11946
+	 */
+	function test_valid_mime() {
+		$this->make_user_by_role( 'editor' );
+
+		// create attachment
+		$filename = ( DIR_TESTDATA . '/images/test-image-mime-jpg.png' );
+		$contents = file_get_contents( $filename );
+		$data = array(
+			'name' => 'test-image-mime-jpg.png',
+			'type' => 'image/png',
+			'bits' => $contents
+		);
+
+		$result = $this->myxmlrpcserver->mw_newMediaObject( array( 0, 'editor', 'editor', $data ) );
+
+		$this->assertNotInstanceOf( 'IXR_Error', $result );
+
+		$this->assertEquals( 'image/jpeg', $result['type'] );
 	}
 }
