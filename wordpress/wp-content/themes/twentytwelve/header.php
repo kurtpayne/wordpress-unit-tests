91bd4a77b167660f79f610f291013a688ff3f203
Kurt Payne <kpayne@gmail.com>
2012-08-02 13:05:43 -0700

    Updating from SVN

--- wordpress/wp-content/themes/twentytwelve/header.php
+++ wordpress/wp-content/themes/twentytwelve/header.php
@@ -23,7 +23,7 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed">
+<div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<hgroup>
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -42,4 +42,4 @@
 		<?php endif; ?>
 	</header><!-- #masthead -->
 
-	<div id="main">
+	<div id="main" class="wrapper">
