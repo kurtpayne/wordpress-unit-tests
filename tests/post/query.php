bc53b15945f9a3291a7f73510c2cec803123794f
Kurt Payne <kpayne@gmail.com>
2012-10-02 13:06:00 -0700

    Updating from SVN

--- tests/post/query.php
+++ tests/post/query.php
@@ -180,4 +180,111 @@ class Tests_Post_Query extends WP_UnitTestCase {
 	$posts = wp_list_pluck( $query->posts, 'ID' );
 	$this->assertEquals( array( $post_id, $post_id4, $post_id5 ), $posts );
     }
+
+    /**
+     * @ticket 16829
+     */
+    function test_meta_default_compare() {
+	// compare should default to IN when meta_value is an array
+	$post_id = $this->factory->post->create();
+	add_post_meta( $post_id, 'foo', 'bar' );
+	$post_id2 = $this->factory->post->create();
+	add_post_meta( $post_id2, 'bar', 'baz' );
+	$post_id3 = $this->factory->post->create();
+	add_post_meta( $post_id3, 'foo', 'baz' );
+	$post_id4 = $this->factory->post->create();
+	add_post_meta( $post_id4, 'baz', 'bar' );
+	$post_id5 = $this->factory->post->create();
+	add_post_meta( $post_id5, 'foo', rand_str() );
+
+	$posts = get_posts( array(
+	    'meta_key' => 'foo',
+	    'meta_value' => array( 'bar', 'baz' )
+	) );
+
+	$this->assertEquals( 2, count( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id3 ), $posts );
+
+	$posts = get_posts( array(
+	    'meta_key' => 'foo',
+	    'meta_value' => array( 'bar', 'baz' ),
+	    'meta_compare' => 'IN'
+	) );
+
+	$this->assertEquals( 2, count( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id3 ), $posts );
+    }
+
+    /**
+     * @ticket 17264
+     */
+    function test_duplicate_posts_when_no_key() {
+	$post_id = $this->factory->post->create();
+	add_post_meta( $post_id, 'city', 'Lorem' );
+	add_post_meta( $post_id, 'address', '123 Lorem St.' );
+	$post_id2 = $this->factory->post->create();
+	add_post_meta( $post_id2, 'city', 'Lorem' );
+	$post_id3 = $this->factory->post->create();
+	add_post_meta( $post_id3, 'city', 'Loren' );
+
+	$args = array(
+	    'meta_query' => array(
+		array(
+		    'value' => 'lorem',
+		    'compare' => 'LIKE'
+		)
+	    )
+	);
+
+	$posts = get_posts( $args );
+	$this->assertEquals( 2, count( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id2 ), $posts );
+    }
+
+    /**
+     * @ticket 15292
+     */
+    function test_empty_meta_value() {
+	$post_id = $this->factory->post->create();
+	add_post_meta( $post_id, 'foo', '0' );
+	add_post_meta( $post_id, 'bar', 0 );
+	$post_id2 = $this->factory->post->create();
+	add_post_meta( $post_id2, 'foo', 1 );
+	$post_id3 = $this->factory->post->create();
+	add_post_meta( $post_id3, 'baz', 0 );
+	$post_id4 = $this->factory->post->create();
+	add_post_meta( $post_id4, 'baz', 0 );
+	$post_id5 = $this->factory->post->create();
+	add_post_meta( $post_id5, 'baz', 0 );
+	add_post_meta( $post_id5, 'bar', '0' );
+	$post_id6 = $this->factory->post->create();
+	add_post_meta( $post_id6, 'baz', 0 );
+
+	$posts = get_posts( array( 'meta_key' => 'foo', 'meta_value' => '0' ) );
+	$this->assertEquals( 1, count ( $posts ) );
+	$this->assertEquals( $post_id, $posts[0]->ID );
+
+	$posts = get_posts( array( 'meta_key' => 'bar', 'meta_value' => '0' ) );
+	$this->assertEquals( 2, count ( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id5 ), $posts );
+
+    	$posts = get_posts( array( 'meta_key' => 'bar', 'meta_value' => 0 ) );
+	$this->assertEquals( 2, count ( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id5 ), $posts );
+
+    	$posts = get_posts( array( 'meta_value' => 0 ) );
+	$this->assertEquals( 5, count ( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id3, $post_id4, $post_id5, $post_id6 ), $posts );
+
+    	$posts = get_posts( array( 'meta_value' => '0' ) );
+	$this->assertEquals( 5, count ( $posts ) );
+	$posts = wp_list_pluck( $posts, 'ID' );
+	$this->assertEquals( array( $post_id, $post_id3, $post_id4, $post_id5, $post_id6 ), $posts );
+    }
 }
