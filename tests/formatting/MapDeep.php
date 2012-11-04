f655872cf3a6de085ef29ea9c1b58b17db999c37
Kurt Payne <kpayne@gmail.com>
2012-11-04 06:05:55 -0700

    Updating from SVN

--- tests/formatting/MapDeep.php
+++ tests/formatting/MapDeep.php
@@ -5,7 +5,7 @@
  */
 class Tests_Formatting_MapDeep extends WP_UnitTestCase {
 	function test_map_deep_with_any_function_over_empty_array_should_return_empty_array() {
-		$this->assertEquals( array(), map_deep( function( $_ ) { return 'baba'; } , array() ) );
+		$this->assertEquals( array(), map_deep( array( $this, 'return_baba' ), array() ) );
 	}
 
 	function test_map_deep_should_map_each_element_of_array_one_level_deep() {
@@ -42,6 +42,10 @@ class Tests_Formatting_MapDeep extends WP_UnitTestCase {
 	function test_map_deep_should_map_each_object_property_of_an_object() {
 		$this->assertEquals( (object)array( 'var0' => 'ababa', 'var1' => (object)array( 'xbaba' ) ),
 			map_deep( array( $this, 'append_baba' ), (object)array( 'var0' => 'a', 'var1' => (object)array( 'x' ) ) ) );
+	}
+
+	function return_baba( $value ) {
+		return 'baba';
 	}
 
 	function append_baba( $value ) {
