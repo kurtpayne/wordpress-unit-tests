f7216831aead3531543fd003d5844c47ff91a217
Kurt Payne <kpayne@gmail.com>
2012-11-27 14:06:03 -0700

    Updating from SVN

---
+++ data/wpcom-themes/journalist/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+	<h2><?php _e('Error 404 - Not Found', 'journalist'); ?></h2>
+	<div class="warning">
+		<p><?php _e('Apologies, but we were unable to find what you were looking for. Perhaps searching will help.', 'journalist'); ?></p>
+	</div>
+
+</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
