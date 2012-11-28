26affd3e35d409651a223cdcfb2b981e8acc0092
Kurt Payne <kpayne@gmail.com>
2012-11-27 19:06:03 -0700

    Updating from SVN

---
+++ data/wpcom-themes/albeo/nav.php
@@ -0,0 +1,4 @@
+<div class="nav">
+ 	<div class="left"><?php next_posts_link(__('Older Entries', 'albeo')) ?></div>
+ 	<div class="right"><?php previous_posts_link(__('Newer Entries', 'albeo')) ?></div>
+</div>
