d174ee88698ef7260535aa1eedcafd0ebd849ca0
Kurt Payne <kpayne@gmail.com>
2012-11-27 18:05:43 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sapphire/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+
+		<h2 class="center">Error 404 - Not Found</h2>
+		Sorry, but what you're looking for isn't here. Perhaps you typed a URL incorrectly or clicked on a broken hyperlink.
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
