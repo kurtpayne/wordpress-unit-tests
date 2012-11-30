7dcbf1e251ac13b11d15ff8c28df4d80f1311775
Kurt Payne <kpayne@gmail.com>
2012-11-29 21:05:39 -0700

    Updating from SVN

---
+++ data/wpcom-themes/hemingway/index.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+
+
+	<div id="primary" class="twocol-stories">
+		<div class="inside">
+			<?php
+				// Here is the call to only make two posts show up on the homepage REGARDLESS of your options in the control panel
+				query_posts('showposts=2');
+			?>
+			<?php if (have_posts()) : ?>
+				<?php $first = true; ?>
+				<?php while (have_posts()) : the_post(); ?>
+					<?php if($count < 2) { ?>
+					<?php $count++; ?>
+					<div class="story<?php if($first == true) echo " first" ?>">
+						<h3><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
+						<?php the_excerpt() ?>
+						<div class="details">
+							Posted at <?php the_time() ?> on <?php the_time(get_option('date_format')) ?> | <?php comments_popup_link('leave a comment', '1 comment', '% comments'); ?> | Filed Under: <?php the_category(', ') ?> | <?php if (is_callable('the_tags')) the_tags('Tagged: ', ', ', ' | '); ?> <span class="read-on"><a href="<?php the_permalink() ?>">read on</a></span>
+						</div>
+					</div>
+					<?php $first = false; ?>
+					<?php } ?>
+				<?php endwhile; ?>
+		</div>
+				
+			<?php else : ?>
+		
+				<h2 class="center">Not Found</h2>
+				<p class="center">Sorry, but you are looking for something that isn't here.</p>
+				<?php include (TEMPLATEPATH . "/searchform.php"); ?>
+		
+			<?php endif; ?>
+				
+			<div class="clear"></div>
+		</div>
+	</div>
+	<!-- [END] #primary -->
+
+
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
