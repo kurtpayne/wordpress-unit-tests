d35a236e1114145bb7eeb7462f9914bf01573d2c
Kurt Payne <kpayne@gmail.com>
2012-12-01 16:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/toni/image.php
@@ -0,0 +1,32 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+	
+<div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	<h3 class="storytitle"><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &raquo; <?php the_title(); ?></h2>
+	<?php edit_post_link(__('Edit This'), '<div class="meta">', '</div>'); ?>
+
+	<div class="storycontent">
+		<p class="attachment"><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'auto' ); ?></a></p>
+		<div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); ?></div>
+		<div class="image-description"><?php if ( !empty($post->post_content) ) the_content(); ?></div>
+
+		<div class="navigation">
+			<div class="alignleft"><?php previous_image_link() ?></div>
+			<div class="alignright"><?php next_image_link() ?></div>
+		</div>
+	</div>
+	
+	<div class="feedback">
+            <?php comments_popup_link(__('Leave a Comment'), __('Comments (1)'), __('Comments (%)')); ?>
+	</div>
+
+</div>
+
+<?php comments_template(); // Get wp-comments.php template ?>
+
+<?php endwhile; else: ?>
+<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
