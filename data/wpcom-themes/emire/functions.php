39cbbb92dd08744d321063ffeb069e9d3d614ecd
Kurt Payne <kpayne@gmail.com>
2012-12-01 13:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/emire/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$themecolors = array(
+	'bg' => '3e3e3e',
+	'text' => '000000',
+	'link' => 'bbbbbb',
+	'border' => '3e3e3e'
+);
+
+$content_width = 418;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
---
+++ data/wpcom-themes/springloaded/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+	<p class="wpcom-credits"><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> Theme: SpringLoaded by <a href="http://the449.com/" rel="designer">the449</a>.</p>
+</div><!-- /footer -->
+
+</div><!-- /wrapper -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
