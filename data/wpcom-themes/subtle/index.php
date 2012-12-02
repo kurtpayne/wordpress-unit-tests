6572a9ec5a24ba2a07753586ca8460f3f5216218
Kurt Payne <kpayne@gmail.com>
2012-12-02 00:05:37 -0700

    Updating from SVN

---
+++ data/wpcom-themes/subtle/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Filename: 		home.php
+Date: 			06-06-25
+Copyright: 		2006, Glued Ideas
+Author: 		Christopher Frazier (cfrazier@gluedideas.com)
+Description: 	Multi-Author Template for WordPress (Subtle)
+Requires:
+*/
+
+include('home.php');
+
+?>
