f5b2c68a3c0a14497f4af19f2a65c6ecba4aa873
Kurt Payne <kpayne@gmail.com>
2012-12-03 12:06:02 -0700

    Updating from SVN

---
+++ data/wpcom-themes/digg3/searchform.php
@@ -0,0 +1,6 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<div>
+	<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+	<input type="submit" id="searchsubmit" value="<?php _e('Search', 'digg3'); ?>" />
+</div>
+</form>
