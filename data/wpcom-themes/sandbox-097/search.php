791bec7f11925ee4f314d1548c39f6fd46164ad1
Kurt Payne <kpayne@gmail.com>
2012-12-03 13:06:11 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sandbox-097/search.php
@@ -0,0 +1,66 @@
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content">
+
+<?php if (have_posts()) : ?>
+
+		<h2 class="page-title"><?php _e('Search Results for:', 'sandbox') ?> <span id="search-terms"><?php echo wp_specialchars(stripslashes($_GET['s']), true); ?></span></h2>
+
+			<div id="nav-above" class="navigation">
+				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox')) ?></div>
+			</div>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+				<h3 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'sandbox'), wp_specialchars(get_the_title(), 1)) ?>" rel="bookmark"><?php the_title() ?></a></h3>
+				<div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s &#8211; %2$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></div>
+				<div class="entry-content">
+<?php the_excerpt(''.__('Read More <span class="meta-nav">&raquo;</span>', 'sandbox').'') ?>
+
+				</div>
+				<div class="entry-meta">
+					<span class="author vcard"><?php printf(__('By %s', 'sandbox'), '<a class="url fn n" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="' . sprintf(__('View all posts by %s', 'sandbox'), $authordata->display_name) . '">'.get_the_author().'</a>') ?></span>
+					<span class="meta-sep">|</span>
+					<span class="cat-links"><?php printf(__('Posted in %s', 'sandbox'), get_the_category_list(', ')) ?></span>
+					<?php if (is_callable('get_the_tag_list')) { ?>
+					<span class="meta-sep">|</span>
+					<span class="tag-links"><?php printf(__('Tags: %s', 'sandbox'), get_the_tag_list(', ')) ?></span>
+					<?php } // get_the_tag_list ?>
+					<span class="meta-sep">|</span>
+<?php edit_post_link(__('Edit', 'sandbox'), "\t\t\t\t\t<span class=\"edit-link\">", "</span>\n\t\t\t\t\t<span class=\"meta-sep\">|</span>\n"); ?>
+					<span class="comments-link"><?php comments_popup_link(__('Leave a Comment', 'sandbox'), __('Comments (1)', 'sandbox'), __('Comments (%)', 'sandbox')) ?></span>
+				</div>
+			</div><!-- .post -->
+
+<?php endwhile; ?>
+
+			<div id="nav-below" class="navigation">
+				<div class="nav-previous"><?php next_posts_link(__('<span class="meta-nav">&laquo;</span> Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts <span class="meta-nav">&raquo;</span>', 'sandbox')) ?></div>
+			</div>
+
+<?php else : ?>
+
+			<div id="post-0" class="post noresults">
+				<h2 class="entry-title"><?php _e('Nothing Found', 'sandbox') ?></h2>
+				<div class="entry-content">
+					<p><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'sandbox') ?></p>
+				</div>
+				<form id="noresults-searchform" method="get" action="<?php bloginfo('home') ?>">
+					<div>
+						<input id="noresults-s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true) ?>" size="40" />
+						<input id="noresults-searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Find', 'sandbox') ?>" />
+					</div>
+				</form>
+			</div><!-- .post -->
+
+<?php endif; ?>
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+
+<?php get_sidebar() ?>
+<?php get_footer() ?>
