6569ef430bc9a51c1eef075e88feaa427e6299f3
Kurt Payne <kpayne@gmail.com>
2012-12-04 14:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/thirteen/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffe3',
+	'border' => 'ffffe3',
+	'text' => '000000',
+	'link' => 'c86c00'
+);
+
+$content_width = 477;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
