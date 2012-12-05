cd60c0dbe62bca78806c3ba240bd448a719a17fc
Kurt Payne <kpayne@gmail.com>
2012-12-05 16:05:55 -0700

    Updating from SVN

---
+++ data/wpcom-themes/journalist-13/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<?php 
+if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+wp_head();
+?>
+</head>
+
+<body>
+<div id="container">
+<div id="skip">
+	<p><a href="#content" title="Skip to site content"><?php _e('Skip to content','journalist-13'); ?></a></p>
+	<p><a href="#search" title="Skip to search" accesskey="s"><?php _e('Skip to search - Accesskey = s','journalist-13'); ?></a></p>
+</div>
+	<h1><a href="<?php bloginfo('url'); ?>/"><?php bloginfo('name'); ?></a></h1>
