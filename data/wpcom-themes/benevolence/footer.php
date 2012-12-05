b09d09135e24c90a305e7f8a2a4b506b5d998049
Kurt Payne <kpayne@gmail.com>
2012-12-04 23:05:52 -0700

    Updating from SVN

---
+++ data/wpcom-themes/benevolence/footer.php
@@ -0,0 +1,16 @@
+	</div>
+
+<div id="footer">
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a><br />
+<a class="footerLink" href="http://www.thoughtmechanics.com/blog/2005/01/03/benevolence/" rel="designer">Benevolence</a> <?php _e('theme by Theron Parlin.','benevolence'); ?>
+<br /><?php _e('Syndicate entries using','benevolence'); ?> <a class="footerLink" href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS','benevolence'); ?>">
+   <abbr title="Really Simple Syndication">RSS</abbr></a> <?php _e('and','benevolence'); ?> <a class="footerLink" href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('Comments (RSS)','benevolence'); ?></a>. <?php _e('This theme contains valid','benevolence'); ?> <a class="footerLink" href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional','benevolence'); ?>"><abbr title="eXtensible HyperText Markup Language">XHTML</abbr></a> <?php _e('and','benevolence'); ?> <a class="footerLink" href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>.
+<br /><br />
+</div>
+
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
