6d63d63cb5e0f98e5eab9003715580bcbbf9b880
Kurt Payne <kpayne@gmail.com>
2012-12-06 07:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/jentri/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'fefcfa',
+	'text' => '000000',
+	'link' => '8e7d6c'
+);
+
+$content_width = 420;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
