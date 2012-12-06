16685ab1c2f60c4fdd6ae4108e4a1f7fd7264f2b
Kurt Payne <kpayne@gmail.com>
2012-12-06 15:06:04 -0700

    Updating from SVN

---
+++ data/wpcom-themes/flower-power/footer.php
@@ -0,0 +1,17 @@
+<!-- footer ................................. -->
+<div id="copyright">
+
+	<p><?php printf(__('Theme: %s by %s', 'flower-power'), 'Flower Power', '<a href="http://www.talkxhtml.com/" rel="designer">TalkXHTML</a>'); ?>
+	<br />
+	<a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com', 'flower-power'); ?></a></p>
+	<?php do_action('wp_footer'); ?>
+
+</div> <!-- /footer -->
+
+<div style="clear:both; background-color:#FFFFFF; font-size:1px; line-height:0px;">&nbsp;</div>
+
+</div> <!-- /container -->
+
+</body>
+
+</html>
