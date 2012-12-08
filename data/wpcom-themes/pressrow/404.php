f796c5653b9f920fa60f7382c3c8b5e4180dbc90
Kurt Payne <kpayne@gmail.com>
2012-12-08 01:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/pressrow/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+		<div id="content_box">
+		
+			<div id="content">
+				<div class="post entry">
+				<h2><?php _e('Sorry, the page you are looking for does not exist'); ?></h2>
+
+				</div>
+			</div>
+
+			<?php get_sidebar(); ?>
+			
+		</div>
+
+<?php get_footer(); ?>
