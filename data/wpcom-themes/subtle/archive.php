1d1e8ace54a91f752a5f0d4f57526a4f006f7f8e
Kurt Payne <kpayne@gmail.com>
2012-12-16 00:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/subtle/archive.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Filename: 		archive.php
+Date: 			06-06-25
+Copyright: 		2006, Glued Ideas
+Author: 		Christopher Frazier (cfrazier@gluedideas.com)
+Description: 	Multi-Author Template for WordPress (Subtle)
+Requires:
+*/
+
+include('search.php');
+
+?>
