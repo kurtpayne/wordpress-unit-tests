96dd83aff7c3a936e91a1c46c5bc80b84c0c90a0
Kurt Payne <kpayne@gmail.com>
2012-12-17 03:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/testing-unstandard/searchform.php
@@ -0,0 +1,5 @@
+<div id="searchWrap">
+<form method="get" id="searchForm" action="<?php bloginfo('home'); ?>/">
+	<input type="text" value="search the archives" onfocus="if (this.value == 'search the archives') {this.value = '';}" onblur="if (this.value == '') {this.value = 'search the archives';}" name="s" id="s" /><input id="searchsubmit" type="image" src="<?php bloginfo('template_directory'); ?>/images/btn-search.gif" alt="Submit" />
+</form>
+</div>
