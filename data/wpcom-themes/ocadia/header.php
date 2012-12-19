0f2e96f55e471d9da3d4b87cef5bf167bde6cf05
Kurt Payne <kpayne@gmail.com>
2012-12-18 22:05:39 -0700

    Updating from SVN

---
+++ data/wpcom-themes/ocadia/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
+	
+
+	<style type="text/css" media="screen">
+		@import url( <?php bloginfo('stylesheet_url'); ?> );
+	</style>
+
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+	
+    <?php wp_get_archives('type=monthly&format=link'); ?>
+
+	<?php 
+	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+	wp_head(); 
+	?>
+</head>
+	
+<body>
+
+<div id="wrapper">
+
+<div id="header">
+<h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+<p><?php bloginfo('description'); ?></p>
+</div>
+
+<div id="content">
