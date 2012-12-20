cb6b8487190b0b755941b053b04a2a6de26c4f5c
Kurt Payne <kpayne@gmail.com>
2012-12-20 09:05:54 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sunburn/searchform.php
@@ -0,0 +1,5 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<div>
+	<input type="text" value="Type &amp; Enter" onfocus="this.value=''; this.style.color='#fff';" onblur="this.style.color='#3F3F3F';" name="s" id="s" />
+</div>
+</form>
