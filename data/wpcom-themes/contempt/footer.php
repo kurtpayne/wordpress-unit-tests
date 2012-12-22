8765085a77e01f51a282bc1657eb4fb1f12b69c0
Kurt Payne <kpayne@gmail.com>
2012-12-21 18:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/contempt/footer.php
@@ -0,0 +1,15 @@
+
+
+<div id="footer">
+	<p>
+	<?php printf(__('Theme %s by %s','contempt'),'Contempt','<a href="http://www.vault9.net" rel="designer">Vault9</a>'); ?>.<br />
+	<a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com','contempt'); ?></a>		
+	</p>
+</div>
+
+</div>
+
+		<?php wp_footer(); ?>
+
+</body>
+</html>
