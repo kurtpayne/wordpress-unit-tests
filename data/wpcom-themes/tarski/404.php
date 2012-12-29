632144cddacaf16276d5c2e09ecf09d3432f72cc
Kurt Payne <kpayne@gmail.com>
2012-12-28 20:05:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/tarski/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<div id="primary">
+	<div class="entry static">
+		<div class="post-meta">
+			<h1 class="post-title" id="error-404">Error 404</h1>
+		</div>
+		
+		<div class="post-content">
+			<p>The page you are looking for does not exist; it may have been moved, or removed altogether. You might want to try the search function. Alternatively, return to the <a href="<?php echo get_settings('home'); ?>">front page</a>.</p>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
