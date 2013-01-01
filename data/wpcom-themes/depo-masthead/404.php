2369c2dc8f63afdb44c5c0bff2d721343a1a6a06
Kurt Payne <kpayne@gmail.com>
2012-12-31 20:06:00 -0700

    Updating from SVN

---
+++ data/wpcom-themes/depo-masthead/404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="group">
+
+	<div class="post">
+		<h2><?php _e('We can\'t find what you\'re looking for!', 'depo-masthead'); ?></h2>
+		
+	</div>
+</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
