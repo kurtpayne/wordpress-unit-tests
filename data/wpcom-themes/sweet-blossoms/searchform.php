3c33adb522a7f7acb0309d0fce6abea0af85e97a
Kurt Payne <kpayne@gmail.com>
2013-01-03 23:05:03 -0700

    Updating from SVN

--- data/wpcom-themes/sweet-blossoms/searchform.php
+++
@@ -1,7 +0,0 @@
-        <center>
-        <form action="<?php bloginfo('home'); ?>/" method="get">
-                        <input class="search" value="<?php the_search_query();  ?>" name="s" id="s" />
-                        <div style="padding:3px 0px 0px 0px;"></div>
-                        <input class="search" type="submit" value="   Search   " id="searchbutton" name="searchbutton" />
-        </form>
-        </center>
