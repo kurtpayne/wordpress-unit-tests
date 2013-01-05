c4120ba9c5ef25d13a042e2048cd6a6657d4df69
Kurt Payne <kpayne@gmail.com>
2013-01-05 09:05:06 -0700

    Updating from SVN

--- data/wpcom-themes/twenty-eight/index.php
+++
@@ -1,8 +0,0 @@
-<?php get_header(); ?>
-<div class="content">
-	<div class="primary">
-		<?php include (TEMPLATEPATH . '/theloop.php'); ?>
-	</div> <!-- close primary -->
-	<?php get_sidebar(); ?>
-</div> <!-- close content -->
-	<?php get_footer(); ?>
