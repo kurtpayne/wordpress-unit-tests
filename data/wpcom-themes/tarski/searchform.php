763e20770c6201503f03babe09c26ba3e0e218a9
Kurt Payne <kpayne@gmail.com>
2013-01-07 17:06:06 -0700

    Updating from SVN

---
+++ data/wpcom-themes/tarski/searchform.php
@@ -0,0 +1,6 @@
+<div class="searchbox">
+	<form method="get" id="searchform" action="/"><fieldset>
+		<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" tabindex="21" />
+		<input type="submit" id="searchsubmit" value="Search" tabindex="22" />
+	</fieldset></form>
+</div>
