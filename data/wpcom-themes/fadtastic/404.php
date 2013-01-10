90c6c570f066fe60f220aecbede95b55db8a229e
Kurt Payne <kpayne@gmail.com>
2013-01-10 13:05:47 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fadtastic/404.php
@@ -0,0 +1,19 @@
+<?php get_header(); ?>
+
+		<div id="content_wrapper">
+			<div id="content">
+			
+			<h1>Error 404 - Page Not Found</h1>
+		
+			<p>Sorry - this page cannot be found. Why not to search our archives?</p>
+			
+			<br />	
+			<?php include("searchform.php") ?>
+						
+			</div>
+		</div>
+			
+	
+	<?php include("sidebar.php") ?>
+
+<?php get_footer(); ?>
