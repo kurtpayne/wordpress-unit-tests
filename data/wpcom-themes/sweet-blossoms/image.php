021bb3e6bdcc15e3e844735d3facd6df842d2f60
Kurt Payne <kpayne@gmail.com>
2013-01-12 02:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sweet-blossoms/image.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+<!-- content ................................. -->
+<div id="main">
+	<div class="main">
+
+<?php if (have_posts()) : ?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+		<h2><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &raquo; <?php the_title(); ?></h2>
+
+		<p class="info"><?php edit_post_link('Edit','<span class="editlink">','</span>'); ?></p>
+
+		<p class="attachment"><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'auto' ); ?></a></p>
+		<div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); ?></div>
+		<div class="image-description"><?php if ( !empty($post->post_content) ) the_content(); ?></div>
+
+		<div class="navigation">
+			<div class="alignleft"><?php previous_image_link() ?></div>
+			<div class="alignright"><?php next_image_link() ?></div>
+		</div>
+
+<?php endwhile; ?>
+
+<?php else : ?>
+
+	<h2>Not Found</h2>
+	<p>"Sorry, but you are looking for something that isn't here.</p>
+
+<?php endif; ?>
+
+<?php comments_template(); ?>
+</div>
+</div> <!-- /content -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
