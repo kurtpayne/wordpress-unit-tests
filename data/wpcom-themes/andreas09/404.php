9c5fadaf42eb80519713be33a314fd9f6179b6c8
Kurt Payne <kpayne@gmail.com>
2013-01-16 13:05:50 -0700

    Updating from SVN

---
+++ data/wpcom-themes/andreas09/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<?php get_sidebar(); ?>
+<?php include (TEMPLATEPATH . '/right-sidebar.php'); ?>
+
+<div id="content">
+	<div class="page">
+		<h2 class="center"><?php _e('Error 404 - Not Found','andreas09') ?></h2>
+	</div>
+</div>
+
+<?php get_footer(); ?>
