75a56c9822c1be54e05592606bbbc2d57952f766
Kurt Payne <kpayne@gmail.com>
2013-01-16 18:05:50 -0700

    Updating from SVN

---
+++ data/wpcom-themes/journalist/footer.php
@@ -0,0 +1,9 @@
+</div>
+
+<div id="footer">
+	<p><a href="http://wordpress.com/" rel="generator"><?php _e('Blog at WordPress.com', 'journalist'); ?></a> &mdash; <?php printf( __('%s theme by %s', 'journalist'), 'Journalist', '<a href="http://lucianmarin.com/" rel="designer">Lucian E. Marin</a>' ); ?></p>
+</div>
+<?php do_action('wp_footer', ''); ?>
+
+</body>
+</html>
