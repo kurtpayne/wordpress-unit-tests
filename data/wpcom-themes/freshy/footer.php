b9e9f2c7525352ade15ec335341e984f7dddc193
Kurt Payne <kpayne@gmail.com>
2013-01-19 06:05:52 -0700

    Updating from SVN

---
+++ data/wpcom-themes/freshy/footer.php
@@ -0,0 +1,17 @@
+
+<hr style="display:none"/>
+
+<div id="footer">
+	<small class="footer_content">
+	<a rel="generator" href="http://wordpress.com/"><?php _e('Blog at WordPress.com', TEMPLATE_DOMAIN); ?></a> &bull; <?php printf(__('%s theme by %s.', TEMPLATE_DOMAIN), 'Freshy', '<a href="http://www.jide.fr/">Jide</a>'); ?>
+	</small>
+	
+</div>
+
+</div> <!--- end of the <div id="page"> (from header.php) -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
+
+		
