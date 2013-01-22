2d116a08a5b85caba9e30998f1b89fca651fe059
Kurt Payne <kpayne@gmail.com>
2013-01-21 23:05:02 -0700

    Updating from SVN

--- data/wpcom-themes/hemingway/blocks/about_page.php
+++
@@ -1,8 +0,0 @@
-
-<?php query_posts('pagename=about'); ?>
-<?php if (have_posts()) : ?>
-<?php while (have_posts()) : the_post(); ?>
-<h2>About <?php edit_post_link('Edit', '<small>(', ')</small>'); ?></h2>
-<?php the_content(); ?>
-<?php endwhile; ?>
-<?php endif; ?>
