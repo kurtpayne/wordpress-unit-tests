793d020d33b500e648f384db70473c417c2c21cd
Kurt Payne <kpayne@gmail.com>
2013-01-22 04:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/grid-focus/footer.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *	@package WordPress
+ *	@subpackage Grid_Focus
+ */
+?>
+<?php include (TEMPLATEPATH . '/footer.strip.php'); ?>
+<div id="footer" class="fix">
+	<p class="left">
+		<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></p>
+	<p class="right">Theme: <a href="http://5thirtyone.com/grid-focus" title="Grid Focus by: Derek Punsalan">Grid Focus</a> by <a href="http://5thirtyone.com" rel="designer">Derek Punsalan</a></p>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
