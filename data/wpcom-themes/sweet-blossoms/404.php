731e607bee0c84c605c04ba976876278faeab58d
Kurt Payne <kpayne@gmail.com>
2013-01-23 09:05:58 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sweet-blossoms/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<!-- content ................................. -->
+<div id="content">
+
+	<h2>Error 404 &ndash; File not Found</h2>
+	
+	<p>Sorry, but the page you were looking for could not be found.</p>
+	
+</div> <!-- /content -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
