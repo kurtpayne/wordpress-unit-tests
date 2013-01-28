d5f13e1d09c6e4bc9485ee76d5b31083760de92d
Kurt Payne <kpayne@gmail.com>
2013-01-28 16:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/chaoticsoul/searchform.php
@@ -0,0 +1,6 @@
+<form method="get" id="searchform" action="/">
+<div>
+	<input type="text" name="s" id="s" value="<?php echo attribute_escape(__('Journal Search&#8230;', 'chaoticsoul')); ?>" />
+	<!-- <input type="submit" id="searchsubmit" value="Search" /> -->
+</div>
+</form>
