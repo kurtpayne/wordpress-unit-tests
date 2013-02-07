27c63fea31e9cfcdbd79137ecd91fb791d66ff9d
Kurt Payne <kpayne@gmail.com>
2013-02-07 09:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/mistylook/footer.php
@@ -0,0 +1,9 @@
+<div id="footer">
+<p><a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com','mistylook'); ?></a></p>
+<p><?php printf(__('Theme: %s by %s.','mistylook'), 'Mistylook', '<a href="http://wpthemes.info/" rel="designer">Sadish</a>'); ?></p>
+<br class="clear" />
+</div><!-- end id:footer -->
+<?php wp_footer();?>
+
+</body>
+</html>
