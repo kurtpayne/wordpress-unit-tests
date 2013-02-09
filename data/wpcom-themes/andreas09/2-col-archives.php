6c0a5a78e1b7c976b5bd1c465bb3a06476f41617
Kurt Payne <kpayne@gmail.com>
2013-02-08 21:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/andreas09/2-col-archives.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: 2 Column Archives List
+*/
+?>
+
+<?php get_header(); ?>
+<?php include (TEMPLATEPATH . '/alt-right-sidebar.php'); ?>
+
+<div id="contentalt">
+	<div id="page">
+		<div class="archives">
+			<h1><?php the_title(); ?></h1>
+			<h2 id="months"><?php _e('Archives by Month:','andreas09'); ?></h2>
+			<ul>
+				<?php wp_get_archives('type=monthly'); ?>
+			</ul>
+			<h2 id="categories"><?php _e('Archives by Category:','andreas09'); ?></h2>
+			<ul>
+				<?php wp_list_cats(); ?>
+			</ul>
+		</div>
+	</div>
+	<?php edit_post_link(__('Edit this entry.','andreas09'), '<p>', '</p>'); ?>
+</div>
+
+<?php get_footer(); ?>
