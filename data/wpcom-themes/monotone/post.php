0ca59884f40610b8951d511aaca5b70d28ecb389
Kurt Payne <kpayne@gmail.com>
2013-02-14 07:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/monotone/post.php
@@ -0,0 +1,34 @@
+<div id="container">
+<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+<div id="postmetadata">
+	<div class="sleeve">
+		<p>By: <cite><?php the_author(); ?></cite></p>
+		<p><small><a href="<?php echo get_month_link(get_the_time('Y'), get_the_time('m')); ?>"><?php the_time('M d Y') ?></a></small></p>
+
+		<?php if ( !is_page() ) { ?> 
+			<p><?php the_tags('tags: ', ', ', '<br />'); ?></p>
+    	
+			<p>Category: <?php the_category(', ') ?></p>
+			<p><?php edit_post_link('Edit This Post', '', ''); ?></p>
+		<?php } else { ?>
+			<p><?php edit_post_link('Edit This Page', '', ''); ?></p>
+		<?php } ?>
+
+		<p><?php comments_popup_link('Leave a Comment &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p>
+	</div>
+</div>
+<div id="post">
+	<div class="sleeve">
+	<?php the_content('Read the rest of this entry &raquo;'); ?>
+	</div>
+</div>
+
+<?php if(is_single()) { ?>
+<div class="navigation">
+	<div class="prev"><?php next_post_link('%link', '&lsaquo;') ?></div>
+	<div class="next"><?php previous_post_link('%link', '&rsaquo;') ?></div>
+</div>
+<?php comments_template();
+} ?>
+</div>
