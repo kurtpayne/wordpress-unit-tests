37fc2cefc6a31a7325f96c49d117cdbe399f9e99
Kurt Payne <kpayne@gmail.com>
2013-02-16 03:05:49 -0700

    Updating from SVN

---
+++ data/wpcom-themes/daydream/footer.php
@@ -0,0 +1,10 @@
+	<div id="footer">
+		<p>
+			Theme: Day Dream by <a href="http://jimwhimpey.com/" rel="designer">Jim Whimpey</a>. 
+			<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>.
+		</p>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
