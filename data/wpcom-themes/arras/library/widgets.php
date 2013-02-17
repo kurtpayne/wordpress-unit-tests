7f8f709e74d369b00a619d58206353d36a28e6a7
Kurt Payne <kpayne@gmail.com>
2013-02-17 13:05:39 -0700

    Updating from SVN

---
+++ data/wpcom-themes/arras/library/widgets.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * This file will eventually place the codes for theme widgets that is
+ * compatible with WordPress 2.8.
+ */
+	
+/* End of file widgets.php */
+/* Location: ./library/widgets.php */
+?>
