aab4a925f5c049563a239458443f2f39ddeb824e
Kurt Payne <kpayne@gmail.com>
2013-02-21 16:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/whiteasmilk/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffff',
+	'text' => '333333',
+	'link' => '0066cc'
+);
+
+$content_width = 400;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
