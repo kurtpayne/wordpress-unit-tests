57fbf39aa9d94fcbc7340c98dfd587cf370db7b8
Kurt Payne <kpayne@gmail.com>
2013-02-22 02:05:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/simpla/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffff',
+	'text' => '000000',
+	'link' => '557799'
+);
+
+$content_width = 480;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+?>
