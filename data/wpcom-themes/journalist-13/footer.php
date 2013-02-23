fc3da190da8c62e686878fb16d5b26808e38e8f6
Kurt Payne <kpayne@gmail.com>
2013-02-23 02:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/journalist-13/footer.php
@@ -0,0 +1,10 @@
+<!-- begin footer -->
+</div>
+	<div id="footer">
+	<p><a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com','journalist-13'); ?></a> <?php _e('Theme:','journalist-13');?> The Journalist <?php _e('by','journalist-13'); ?> <a href="http://lucianmarin.com/" rel="designer">Lucian Marin</a></p>
+	</div>
+
+<?php do_action('wp_footer', ''); ?>
+
+</body>
+</html>
