590599047617a34689f60706bc141acceaed50fa
Kurt Payne <kpayne@gmail.com>
2013-02-25 08:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/dusk/footer.php
@@ -0,0 +1,17 @@
+	<div id="footer">
+		<p>
+		<a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional'); ?>"><?php _e('<abbr title="eXtensible HyperText Markup Language">XHTML</abbr>'); ?></a>
+		&#183;
+		<a href="http://jigsaw.w3.org/css-validator/check/referer" title="<?php _e('This page validates as CSS'); ?>"><?php _e('<abbr title="Cascading Style Sheets">CSS</abbr>'); ?></a>
+		&#183;
+		Theme: Dusk by <a href="http://beccary.com" rel="designer">Beccary</a>. <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+		</p>
+	</div>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
+
