346f6077a96374554c00786ae3b1191625eac2a7
Kurt Payne <kpayne@gmail.com>
2013-02-25 12:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/classic/sidebar.php
@@ -0,0 +1,49 @@
+
+<!-- begin sidebar -->
+<div id="menu">
+
+<ul>
+<?php if ( function_exists('dynamic_sidebar') && dynamic_sidebar() ) : ?>
+
+</ul>
+
+</div>
+
+<?php return; ?>
+
+<?php endif; ?>
+
+	<?php wp_list_pages('title_li=' . __('Pages:')); ?>
+	<?php wp_list_bookmarks('title_after=&title_before='); ?>
+	<?php wp_list_categories('title_li=' . __('Categories:')); ?>
+ <li id="search">
+   <label for="s"><?php _e('Search:','classic'); ?></label>
+   <form id="searchform" method="get" action="<?php bloginfo('home'); ?>/">
+	<div>
+		<input type="text" name="s" id="s" size="15" /><br />
+		<input type="submit" value="<?php _e('Search','classic'); ?>" />
+	</div>
+	</form>
+ </li>
+ <li id="archives"><?php _e('Archives:','classic'); ?>
+ 	<ul>
+	 <?php wp_get_archives('type=monthly'); ?>
+ 	</ul>
+ </li>
+ <li id="meta"><?php _e('Meta:','classic'); ?>
+ 	<ul>
+		<?php wp_register(); ?>
+		<li><?php wp_loginout(); ?></li>
+		<li><a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate this site using RSS','classic'); ?>"><?php _e('<abbr title="Really Simple Syndication">RSS</abbr>','classic'); ?></a></li>
+		<li><a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php _e('The latest comments to all posts in RSS','classic'); ?>"><?php _e('Comments <abbr title="Really Simple Syndication">RSS</abbr>','classic'); ?></a></li>
+		<li><a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional','classic'); ?>"><?php _e('Valid <abbr title="eXtensible HyperText Markup Language">XHTML</abbr>','classic'); ?></a></li>
+		<li><?php _e('<a href="http://gmpg.org/xfn/"><abbr title="XHTML Friends Network">XFN</abbr></a>','classic'); ?></li>
+		<li><?php _e('<a href="http://wordpress.com/">WordPress.com</a>','classic'); ?></li>
+		<?php wp_meta(); ?>
+	</ul>
+ </li>
+
+</ul>
+
+</div>
+<!-- end sidebar -->
