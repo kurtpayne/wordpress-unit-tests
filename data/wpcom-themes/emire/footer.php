55255698db436ab9195edded0a6b22c94af767e4
Kurt Payne <kpayne@gmail.com>
2013-02-24 19:05:02 -0700

    Updating from SVN

--- data/wpcom-themes/emire/footer.php
+++
@@ -1,4 +0,0 @@
-</div>
-<div id="footer"><?php _e('Theme: Emire by','emire'); ?> <a href="http://ifelse.co.uk" rel="designer">Phu</a>. <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></div>
-
-<?php wp_footer(); ?>
