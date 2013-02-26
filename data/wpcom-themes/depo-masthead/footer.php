b11749b5ff6f902799c0c920fdbfab823988e624
Kurt Payne <kpayne@gmail.com>
2013-02-25 23:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/depo-masthead/footer.php
@@ -0,0 +1,11 @@
+	<div id="footer">
+		<p>
+			<a href="http://wordpress.com/" rel="generator">Blog at WordPress.com</a> Theme: <strong>DePo Masthead</strong> by <a href="http://powazek.com" rel="designer">Derek Powazek</a>
+			
+		</p>
+	</div>
+</div>
+<?php wp_footer(); ?>
+
+</body>
+</html>
