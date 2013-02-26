a625e00593bbce5bc35ec003c81ecb9b1c3b9404
Kurt Payne <kpayne@gmail.com>
2013-02-25 21:05:46 -0700

    Updating from SVN

---
+++ data/wpcom-themes/neat/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<h2 class="center"><?php _e('Error 404 - Not Found.'); ?></h2>
+		&nbsp;<?php _e('Search something maybe?'); ?> <?php include (TEMPLATEPATH . '/searchform.php'); ?>
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
