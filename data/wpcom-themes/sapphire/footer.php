730a1086b2d0837a9da3bb4d9b17ce32e4445fb4
Kurt Payne <kpayne@gmail.com>
2013-02-26 21:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sapphire/footer.php
@@ -0,0 +1,12 @@
+
+<hr />
+<div id="footer">
+	<p>
+		<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>. Theme: Sapphire by <a href="http://www.michaelmartine.com/" rel="designer">Michael Martine</a>.
+	</p>
+</div>
+</div>
+		<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
