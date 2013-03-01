dffbbf0c6d106aabc267e6a9e15a45233f658987
Kurt Payne <kpayne@gmail.com>
2013-03-01 13:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/grid-focus/page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *	@package WordPress
+ *	@subpackage Grid_Focus
+ */
+get_header();
+?>
+<div id="filler" class="fix">
+	<div id="mainColumn" class="fix"><a name="main"></a>
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<div class="postMeta">
+				<p class="container">
+					<span class="date">&nbsp;<?php edit_post_link(' (Edit)', '', ''); ?></span>
+				</p>
+			</div>
+			<h2><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title() ?></a></h2>
+			<div class="entry">
+				<p><?php the_content('<p class="serif">Read the rest of this entry &raquo;</p>'); ?></p>
+				<p><?php wp_link_pages(); ?></p>
+			</div>
+		</div>
+		<div id="commentsContainer">
+			<?php comments_template(); ?>
+		</div>
+		<?php endwhile; else: ?>
+		<?php endif; ?>
+	</div>
+	<?php include (TEMPLATEPATH . '/second.column.post.php'); ?>
+	<?php include (TEMPLATEPATH . '/third.column.shared.php'); ?>
+</div>
+<?php get_footer(); ?>
