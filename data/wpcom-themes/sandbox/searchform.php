ff251afcdb33264ff45c25b00e7d1fefa9082af4
Kurt Payne <kpayne@gmail.com>
2013-03-02 08:05:03 -0700

    Updating from SVN

--- data/wpcom-themes/sandbox/searchform.php
+++
@@ -1,6 +0,0 @@
-<form id="searchform" method="get" action="<?php bloginfo('home') ?>">
-	<div>
-		<input id="s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true); ?>" size="40" />
-		<input id="searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Search &raquo;', 'sandbox') ?>" />
-	</div>
-</form>
