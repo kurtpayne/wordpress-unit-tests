9eda42169437f1535349cf3ef4569e0bb2bd6ad7
Kurt Payne <kpayne@gmail.com>
2013-03-02 07:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/daydream/searchform.php
@@ -0,0 +1,9 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+
+	<div style="text-align: center;">
+	
+		<input type="text" value="Type and Press Enter to Search..." onfocus="this.value=''; this.style.color='#000';" onblur="this.value='Type and Press Enter to Search...'; this.style.color='#ccc';"  name="s" id="s" />
+	
+	</div>
+
+</form>
