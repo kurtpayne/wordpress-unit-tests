7c3fd5d6b1fdfb5a28f005f1211490d02d9a0ddb
Kurt Payne <kpayne@gmail.com>
2013-03-05 02:05:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/journalist/functions.php
@@ -0,0 +1,44 @@
+<?php
+$content_width = 700;
+
+if ( function_exists('register_sidebar') )
+    register_sidebar(array(
+        'before_widget' => '',
+        'after_widget' => '',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>',
+    ));
+
+$themecolors = array(
+	'bg' => 'fff',
+	'border' => '777',
+	'text' => '1c1c1c',
+	'link' => '004276',
+);
+
+function tj_comment_class( $classname='' ) {
+	global $comment, $post;
+
+	$c = array();
+	if ($classname)
+		$c[] = $classname;
+
+	// Collects the comment type (comment, trackback),
+	$c[] = $comment->comment_type;
+
+	// If the comment author has an id (registered), then print the log in name
+	if ( $comment->user_id > 0 ) {
+		$user = get_userdata($comment->user_id);
+
+		// For all registered users, 'byuser'; to specificy the registered user, 'commentauthor+[log in name]'
+		$c[] = "byuser comment-author-" . sanitize_title_with_dashes(strtolower($user->user_login));
+		// For comment authors who are the author of the post
+		if ( $comment->user_id === $post->post_author )
+			$c[] = 'bypostauthor';
+	}
+
+	// Separates classes with a single space, collates classes for comment LI
+	return join(' ', apply_filters('comment_class', $c));
+}
+
+?>
