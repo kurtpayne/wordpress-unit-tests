e7d0a5aa5c114c93bea4dabb8f10d166be340340
Kurt Payne <kpayne@gmail.com>
2013-03-05 00:05:02 -0700

    Updating from SVN

--- data/wpcom-themes/solipsus/searchform.php
+++
@@ -1,5 +0,0 @@
-<center><form method="get" action="/">
-<p>
-<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" /> <input type="submit" value="<?php _e('Search'); ?>" />
-</p>
-</form></center>
