551586d587f2e5639b9826e685934f33c1e44103
Kurt Payne <kpayne@gmail.com>
2013-03-06 12:05:44 -0700

    Updating from SVN

---
+++ wordpress/wp-content/themes/twentythirteen/js/theme-customizer.js
@@ -0,0 +1,26 @@
+/**
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ * Things like site title and description changes.
+ */
+
+( function( $ ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+	// Header text color.
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title, .site-description' ).css( 'color', to );
+		} );
+	} );
+} )( jQuery );
