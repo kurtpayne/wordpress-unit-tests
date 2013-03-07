504d9fad4d6a129826527416d0d02067a81c21b6
Kurt Payne <kpayne@gmail.com>
2013-03-07 16:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/neo-sapien-05/searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+<input type="submit" id="searchsubmit" value="Search" />
+</form>
