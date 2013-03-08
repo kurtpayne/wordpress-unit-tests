3bee7ee80f513d9bae8a5172ed960712cb83b0eb
Kurt Payne <kpayne@gmail.com>
2013-03-08 00:06:05 -0700

    Updating from SVN

---
+++ data/wpcom-themes/steira/single.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	
+	<head>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		
+		<title>Maxvoltar | Who needs a CMS?</title>
+		
+		<link rel="stylesheet" href="css/master.css" type="text/css" />
+		
+		<!--[if lt IE 7]>
+			
+			<link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" />
+		
+		<![endif]-->
+		
+	</head>
+	
+	<body>
+		
+		<div id="wrapper">
+			
+			<p id="skiptocontent"><a href="#content">Skip to content</a></p>
+			
+			<div id="masthead">
+			
+				<ul id="navigation">
+					<li>
+						<a href="index.html">Homepage</a>
+					</li>
+					<li class="current">
+						<a href="post.html">Archive</a>
+					</li>
+					<li>
+						<a href="about.html">About</a>
+					</li>
+				</ul>
+				
+				<h1><a href="#">Maxvoltar</a></h1>
+				
+			</div><!-- masthead -->
+			
+			<div id="subhead">
+				
+				<form>
+					
+					<p>
+						<label for="q">Search</label>
+						<input class="text" id="q" type="text" value="Search&hellip;" onfocus="if(this.value == 'Search&hellip;'){this.value = ''}" onblur="if(this.value == ''){this.value = 'Search&hellip;'}" />
+						<input class="submit" type="submit" value="Go" />
+					</p>
+					
+				</form>
+				
+				<blockquote>
+					
+					<p><span class="quote">&ldquo;</span>Just another WordPress theme<span class="quote">&rdquo;</span></p>
+					
+				</blockquote>
+				
+			</div><!-- subhead -->
+			
+			<div id="body">
+				
+				<div id="content">
+					
+					<h2 class="haslink">
+						<a href="#">
+							<span class="posted">22/03/08</span>
+							<span class="new">New</span>
+							<span class="title">Who needs a CMS?</span>
+						</a>
+					</h2>
+					
+					<div class="contentblock">
+						
+						<div id="intro">
+							
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam placerat. Nulla imperdiet.</p>
+							
+						</div><!-- intro -->
+						
+						<h3>A header type 3</h3>
+					
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+						<div class="image">
+							
+							<p>
+								<a href="http://wordpress.org/development/2008/03/wordpress-25-brecker/"><img src="temp/wordpress20.jpg" alt="WordPress 2.0 admin interface" /></a><br />
+								WordPress 2.0 admin interface
+							</p>
+							
+						</div><!-- image -->
+						
+						<h4>A header type 4</h4>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+						<ul>
+							<li>Lorem <a href="#">ipsum</a> dolor sit amet</li>
+							<li><strong>Consectetuer adipiscing elit</strong></li>
+							<li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</li>
+							<li>Nulla imperdiet</li>
+						</ul>
+						
+						<h5>A header type 5</h5>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+						<ol>
+							<li>Lorem ipsum dolor sit amet</li>
+							<li>Consectetuer adipiscing elit</li>
+							<li>Etiam placerat</li>
+							<li>Nulla imperdiet</li>
+						</ol>
+						
+						<h6>A header type 6</h6>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+						<blockquote>
+							
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</p>
+							
+						</blockquote>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+						<table cellspacing="0">
+							<thead>
+								<tr>
+									<th>#</th>
+									<th>Tableheader 1</th>
+									<th>Tableheader 2</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Tabledata 1</td>
+									<td>Tabledata 2</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>Tabledata 1</td>
+									<td>Tabledata 2</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>Tabledata 1</td>
+									<td>Tabledata 2</td>
+								</tr>
+							</tbody>
+						</table>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+					</div><!-- contentblock -->
+					
+					<ul class="postdetails alt">
+						<li class="comments"><a href="#">5 comments</a></li>
+						<li class="categories">Posted under <a href="#">CMS</a>, <a href="#">development</a> &amp; <a href="#">WordPress</a>.</li>
+					</ul><!-- postdetails -->
+					
+				</div><!-- content -->
+				
+				<div id="sidebar">
+					
+					<h3 id="comments">
+						<span class="more"><a href="#newcomment"><label for="name">Write a new comment</label></a></span>
+						<span class="title">5 Comments</span>
+					</h3>
+					
+					<div id="c0001" class="comment">
+					
+						<h4>
+							<a class="permlink" href="#c0001" title="23/03/2008 - 9:12AM"># 1</a>
+							<span class="name"><a href="#"><img src="img/gravatar.gif" alt="Get a Gravatar!" />Judas Priest</a></span> said
+						</h4>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</p>
+						
+					</div><!-- comment -->
+					
+					<div id="c0002" class="comment">
+					
+						<h4>
+							<a class="permlink" href="#c0002" title="23/03/2008 - 12:55PM"># 2</a>
+							<span class="name"><img src="img/gravatar.gif" alt="Get a Gravatar!" />Antigone</span> said
+						</h4>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</p>
+						
+					</div><!-- comment -->
+					
+					<div id="c0003" class="comment">
+					
+						<h4>
+							<a class="permlink" href="#c0003" title="23/03/2008 - 9:12AM"># 3</a>
+							<span class="name"><a href="#"><img src="temp/mattdamon.jpg" alt="Get a Gravatar!" />Matt Damon</a></span> said
+						</h4>
+						
+						<p>Lorem ipsum dolor sit amet.</p>
+						
+					</div><!-- comment -->
+					
+					<div id="c0004" class="comment">
+					
+						<h4>
+							<a class="permlink" href="#c0004" title="23/03/2008 - 9:12AM"># 4</a>
+							<span class="name"><a href="#"><img src="img/gravatar.gif" alt="Get a Gravatar!" />Judas Priest</a></span> said
+						</h4>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</p>
+						
+					</div><!-- comment -->
+					
+					<div id="c0005" class="comment">
+					
+						<h4>
+							<a class="permlink" href="#c0005" title="23/03/2008 - 12:55PM"># 5</a>
+							<span class="name"><img src="img/gravatar.gif" alt="Get a Gravatar!" />Antigone</span> said
+						</h4>
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet.</p>
+						
+					</div><!-- comment -->
+					
+					<h3 id="newcomment">
+						<span class="title"><strong>New</strong> comment</span>
+					</h3>
+					
+					<form id="newcommentform">
+						
+						<p>
+							<label for="name">Name</label>
+							<input class="text" type="text" id="name" />
+						</p>
+						
+						<p>
+							<label for="email">Email</label>
+							<input class="text" type="text" id="email" />
+						</p>
+						
+						<p>
+							<label for="website">Website <small>optional</small></label>
+							<input class="text" type="text" id="website" />
+						</p>
+						
+						<p>
+							<label for="message">Message</label>
+							<textarea id="message"></textarea>
+						</p>
+						
+						<p class="buttons">
+							<input class="submit" type="submit" value="submit" />
+						</p>
+						
+					</form><!-- newcommentform -->
+					
+				</div><!-- sidebar -->
+				
+			</div><!-- body -->
+			
+			<div id="footer">
+				
+				<p id="copyrights">Don&rsquo;t steal things that aren&rsquo;t yours.<br />
+					Nice people put a lot of effort in them.</p>
+				
+					<p id="credits">You&rsquo;re looking at a template called <span class="quote">&ldquo;</span><a href="http://www.madebyelephant.com/work/steira">Steira</a><span class="quote">&rdquo;</span>.<br />
+				Designed by <a href="http://www.madebyelephant.com/">Made by Elephant</a>, powered by <a href="http://www.wordpress.com/">WordPress</a>.</p>
+				
+			</div><!-- footer -->
+			
+		</div><!-- wrapper -->
+		
+	</body>
+	
+</html>
