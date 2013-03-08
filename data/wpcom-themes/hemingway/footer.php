0ca2aa8b5e2c8f82287815560325c5349913e97e
Kurt Payne <kpayne@gmail.com>
2013-03-07 19:05:38 -0700

    Updating from SVN

---
+++ data/wpcom-themes/hemingway/footer.php
@@ -0,0 +1,16 @@
+
+<hr class="hide" />
+	<div id="footer">
+		<div class="inside">
+			<?php
+				// You are not required to keep this link back to Warpspire, but if you wouldn't mind, leaving it in would make my day.
+			?>
+			<p class="copyright"><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: <a href="http://warpspire.com/hemingway" rel="designer">Hemingway</a>.</p>
+			<p class="attributes"><a href="<?php bloginfo('rss2_url'); ?>">Entries RSS</a> <a href="<?php bloginfo('comments_rss2_url'); ?>">Comments RSS</a></p>
+		</div>
+	</div>
+	<!-- [END] #footer -->	
+
+	<?php wp_footer(); ?>
+</body>
+</html>
