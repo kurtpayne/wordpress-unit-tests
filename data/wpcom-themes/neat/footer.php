945442d338385fdbe14c57afdc3efbceb2021ccd
Kurt Payne <kpayne@gmail.com>
2013-03-08 07:05:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/neat/footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+	<p>
+	<a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com'); ?></a>
+		<br />Theme: Neat. 
+		<a href="<?php bloginfo('rss2_url'); ?>"><?php _e('Entries (RSS)'); ?></a>
+		and <a href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('Comments (RSS)'); ?></a>.
+	</p>
+</div>
+
+</div>
+		<?php do_action('wp_footer'); ?>
+<br /><br /><br />
+</body>
+</html>
