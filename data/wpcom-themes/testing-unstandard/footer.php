509bb52ab51d1e26075eb2e484b357db83106f8c
Kurt Payne <kpayne@gmail.com>
2013-03-09 20:05:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/testing-unstandard/footer.php
@@ -0,0 +1,7 @@
+<div id="footer">
+  <div class="left"><a href="<?php echo get_settings('home'); ?>/" title="Home"><?php bloginfo('name'); ?></a> - Theme: <a href="http://5thirtyone.com/the-unstandard" title="Download The Unstandard theme">The Unstandard</a> by <a href="http://is.derekpunsalan.com" title="Derek Punsalan">Derek Punsalan</a></div>
+</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ wordpress/wp-content/themes/twentytwelve/js/theme-customizer.js
@@ -0,0 +1,52 @@
+/**
+ * Theme Customizer enhancements for a better user experience.
+ *
+ * Contains handlers to make Theme Customizer preview reload changes asynchronously.
+ * Things like site title, description, and background color changes.
+ */
+
+( function( $ ) {
+	// Site title and description.
+	wp.customize( 'blogname', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a' ).text( to );
+		} );
+	} );
+	wp.customize( 'blogdescription', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-description' ).text( to );
+		} );
+	} );
+	// Header text color
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a, .site-description' ).css( 'color', to );
+		} );
+	} );
+
+	// Hook into background color/image change and adjust body class value as needed.
+	wp.customize( 'background_color', function( value ) {
+		value.bind( function( to ) {
+			var body = $( 'body' );
+
+			if ( ( '#ffffff' == to || '#fff' == to ) && 'none' == body.css( 'background-image' ) )
+				body.addClass( 'custom-background-white' );
+			else if ( '' == to && 'none' == body.css( 'background-image' ) )
+				body.addClass( 'custom-background-empty' );
+			else
+				body.removeClass( 'custom-background-empty custom-background-white' );
+		} );
+	} );
+	wp.customize( 'background_image', function( value ) {
+		value.bind( function( to ) {
+			var body = $( 'body' );
+
+			if ( '' != to )
+				body.removeClass( 'custom-background-empty custom-background-white' );
+			else if ( 'rgb(255, 255, 255)' == body.css( 'background-color' ) )
+				body.addClass( 'custom-background-white' );
+			else if ( 'rgb(230, 230, 230)' == body.css( 'background-color' ) && '' == _wpCustomizeSettings.values.background_color )
+				body.addClass( 'custom-background-empty' );
+		} );
+	} );
+} )( jQuery );
