f52779dd85109db9e66f8eeb29949e57d1ed3f8a
Kurt Payne <kpayne@gmail.com>
2013-03-13 01:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/chaostheory/footer.php
@@ -0,0 +1,10 @@
+	<div id="footer">
+		<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> Theme: ChaosTheory.
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
