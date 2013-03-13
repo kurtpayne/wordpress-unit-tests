839353b15d526c59307aafc10d480403c2a6f988
Kurt Payne <kpayne@gmail.com>
2013-03-13 05:05:38 -0700

    Updating from SVN

---
+++ data/wpcom-themes/andreas04/footer.php
@@ -0,0 +1,16 @@
+
+<div id="footer">
+  <h2 class="hide">Site info</h2>
+  <span><?php bloginfo('name'); ?></span><br />
+  Theme: Andreas04 <?php _e('by','andreas04'); ?> <a href="http://andreasviklund.com/" rel="designer">Andreas Viklund</a>. <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>.
+</div>
+
+
+    </div>
+  
+  </div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
