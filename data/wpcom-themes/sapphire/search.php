f35f093e13c12c581ec2f0376fac407a3864b345
Kurt Payne <kpayne@gmail.com>
2013-03-14 09:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sapphire/search.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+
+	<?php if (have_posts()) : ?>
+
+		<h2 class="pagetitle">Search Results</h2>
+		
+		<div class="navigation">
+			<div class="alignleft"><?php posts_nav_link('','','&laquo; Previous Entries') ?></div>
+			<div class="alignright"><?php posts_nav_link('','Next Entries &raquo;','') ?></div>
+		</div>
+
+
+		<?php while (have_posts()) : the_post(); ?>
+				
+			<div <?php post_class(); ?>>
+				<h3 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h3>
+				<small><?php the_time('l, F jS, Y') ?></small>
+				
+				<div class="entry">
+					<?php the_excerpt() ?>
+				</div>
+		
+				<p class="postmetadata">Posted in <?php the_category(', ') ?> <br /><?php the_tags('<strong>Tags:</strong> ', ', ', '<br />'); ?><strong>|</strong> <?php edit_post_link('Edit','','<strong>|</strong>'); ?>  <?php comments_popup_link('Leave a Comment &#187;', '1 Comment &#187;', '% Comments &#187;'); ?></p> 
+
+			</div>
+	
+		<?php endwhile; ?>
+
+		<div class="navigation">
+			<div class="alignleft"><?php posts_nav_link('','','&laquo; Previous Entries') ?></div>
+			<div class="alignright"><?php posts_nav_link('','Next Entries &raquo;','') ?></div>
+		</div>
+	
+	<?php else : ?>
+
+		<h2 class="center">Not Found</h2>
+		<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+
+	<?php endif; ?>
+		
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
