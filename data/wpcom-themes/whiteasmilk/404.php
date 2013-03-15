361e76b77816dbc4ac57403131de9f7315c71275
Kurt Payne <kpayne@gmail.com>
2013-03-14 23:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/whiteasmilk/404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+	<div id="content" class="narrowcolumn">
+
+		<h2 class="center"><?php _e('Error 404 - Not Found','whiteasmilk'); ?></h2>
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
