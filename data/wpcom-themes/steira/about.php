775c11fe42735efe5e04eddad91a13fc2e14eec2
Kurt Payne <kpayne@gmail.com>
2013-03-17 02:05:39 -0700

    Updating from SVN

---
+++ data/wpcom-themes/steira/about.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	
+	<head>
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+		
+		<title>Maxvoltar | About</title>
+		
+		<link rel="stylesheet" href="css/master.css" type="text/css" />
+		
+		<!--[if lt IE 7]>
+			
+			<link rel="stylesheet" type="text/css" href="css/ie6.css" media="screen" />
+		
+		<![endif]-->
+		
+	</head>
+	
+	<body>
+		
+		<div id="wrapper">
+			
+			<p id="skiptocontent"><a href="#content">Skip to content</a></p>
+			
+			<div id="masthead">
+			
+				<ul id="navigation">
+					<li>
+						<a href="index.html">Homepage</a>
+					</li>
+					<li>
+						<a href="post.html">Archive</a>
+					</li>
+					<li class="current">
+						<a href="about.html">About</a>
+					</li>
+				</ul>
+				
+				<h1><a href="#">Maxvoltar</a></h1>
+				
+			</div><!-- masthead -->
+			
+			<div id="subhead">
+				
+				<form>
+					
+					<p>
+						<label for="q">Search</label>
+						<input class="text" id="q" type="text" value="Search&hellip;" onfocus="if(this.value == 'Search&hellip;'){this.value = ''}" onblur="if(this.value == ''){this.value = 'Search&hellip;'}" />
+						<input class="submit" type="submit" value="Go" />
+					</p>
+					
+				</form>
+				
+				<blockquote>
+					
+					<p><span class="quote">&ldquo;</span>Just another WordPress theme<span class="quote">&rdquo;</span></p>
+					
+				</blockquote>
+				
+			</div><!-- subhead -->
+			
+			<div id="body">
+				
+				<div id="content">
+					
+					<h2 class="haslink">
+						<a href="#">
+							<span class="title">About</span>
+						</a>
+					</h2>
+					
+					<div class="contentblock">
+						
+						<div id="intro">
+							
+							<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Etiam placerat. Nulla imperdiet.</p>
+							
+						</div><!-- intro -->
+						
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam placerat. Nulla imperdiet. Pellentesque habitant <strong>morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Aliquam in massa sit amet nulla auctor <em>imperdiet</em>. Donec ac <a href="#">augue in turpis</a> vulputate egestas. Nullam interdum. Nulla adipiscing justo id sapien. Etiam tempor. Mauris et mauris. Phasellus ullamcorper ante ac neque.</p>
+						
+					</div><!-- contentblock -->
+					
+				</div><!-- content -->
+				
+				<div id="sidebar">
+					
+					<h2 class="haslink">
+						<a href="#">
+							<span class="more">Archive &rsaquo;</span>
+							<span class="title">Recent <strong>posts</strong></span>
+						</a>
+					</h2>
+					
+					<div class="sidebarblock">
+					
+						<ul id="recentpostslist" class="indexlist">
+							<li>
+								<a href="#">
+									<span class="date">22/03/08</span>
+									<span class="title">Who needs a CMS?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="date">20/02/08</span>
+									<span class="title">Are we alone in the universe?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="date">29/01/08</span>
+									<span class="title">Why do we Live?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="date">25/12/07</span>
+									<span class="title">Venus / Mars</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="date">06/12/07</span>
+									<span class="title">Hello world</span>
+								</a>
+							</li>
+						</ul><!-- recentpostslist -->
+						
+					</div><!-- sidebarblock -->
+					
+					<h2>Post <strong>categories</strong></h2>
+					
+					<div class="sidebarblock" id="categories">
+					
+						<ul id="categoriesgrid">
+							<li>
+								<a href="#">CMS</a>
+							</li>
+							<li>
+								<a href="#">Development</a>
+							</li>
+							<li>
+								<a href="#">Philosophy</a>
+							</li>
+							<li>
+								<a href="#">Geektalk</a>
+							</li>
+							<li>
+								<a href="#">Skateboarding</a>
+							</li>
+							<li>
+								<a href="#">Webdesign</a>
+							</li>
+							<li>
+								<a href="#">Webstandards</a>
+							</li>
+							<li>
+								<a href="#">WordPress</a>
+							</li>
+							<li>
+								<a href="#">Apple</a>
+							</li>
+							<li>
+								<a href="#">Gadgets</a>
+							</li>
+							<li>
+								<a href="#">Traveling</a>
+							</li>
+						</ul><!-- categoriesgrid -->
+						
+					</div><!-- sidebarblock -->
+					
+					<h2>Recent <strong>comments</strong></h2>
+					
+					<div class="sidebarblock">
+					
+						<ul id="recentcommentslist" class="indexlist">
+							<li>
+								<a href="#">
+									<span class="name">Judas Priest</span>
+									<span class="didwhat">commented on</span>
+									<span class="title">Who needs a CMS?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="name">Metallica</span>
+									<span class="didwhat">commented on</span>
+									<span class="title">Why do we Live?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="name">Flogging Molly</span>
+									<span class="didwhat">commented on</span>
+									<span class="title">Are we alone in the universe?</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="name">Berzerker</span>
+									<span class="didwhat">commented on</span>
+									<span class="title">Venus / Mars</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<span class="name">Antigone</span>
+									<span class="didwhat">commented on</span>
+									<span class="title">Hello world</span>
+								</a>
+							</li>
+						</ul><!-- recentcommentslist -->
+						
+					</div><!-- sidebarblock -->
+					
+				</div><!-- sidebar -->
+				
+			</div><!-- body -->
+			
+			<div id="footer">
+				
+				<p id="copyrights">Don&rsquo;t steal things that aren&rsquo;t yours.<br />
+					Nice people put a lot of effort in them.</p>
+				
+					<p id="credits">You&rsquo;re looking at a template called <span class="quote">&ldquo;</span><a href="http://www.madebyelephant.com/work/steira">Steira</a><span class="quote">&rdquo;</span>.<br />
+				Designed by <a href="http://www.madebyelephant.com/">Made by Elephant</a>, powered by <a href="http://www.wordpress.com/">WordPress</a>.</p>
+				
+			</div><!-- footer -->
+			
+		</div><!-- wrapper -->
+		
+	</body>
+	
+</html>
