a52a322219f162390d377cc522f61a1af3ca10ca
Kurt Payne <kpayne@gmail.com>
2013-03-18 09:05:46 -0700

    Updating from SVN

---
+++ data/wpcom-themes/solipsus/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'bebcad',
+	'border' => 'bebcad',
+	'text' => '000000',
+	'link' => '5785a4'
+);
+
+$content_width = 390;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
