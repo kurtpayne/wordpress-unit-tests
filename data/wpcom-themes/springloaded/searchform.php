db39bb610a2985c2bd8279ffdebb8923d463000d
Kurt Payne <kpayne@gmail.com>
2013-03-20 08:05:04 -0700

    Updating from SVN

--- data/wpcom-themes/springloaded/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" action="<?php bloginfo('url'); ?>/">
-<p><input type="text" name="s" onblur="this.value=(this.value=='') ? '<?php _e('Search this Blog', 'springloaded'); ?>' : this.value;" onfocus="this.value=(this.value=='<?php _e('Search this Blog', 'springloaded'); ?>') ? '' : this.value;" value="<?php _e('Search this Blog', 'springloaded'); ?>" id="s" />
-<input type="submit" name="submit" value="Search" id="some_name"></p>
-</form>
