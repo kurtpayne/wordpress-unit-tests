45d3156333ef488de2db743be480a0e8974f5ef1
Kurt Payne <kpayne@gmail.com>
2013-03-20 04:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/vermilion-christmas/footer.php
@@ -0,0 +1,16 @@
+<!-- BEGIN FOOTER.PHP -->
+</div><!-- end id="page2" -->
+<div id="footer">
+<div id="footer-wrap">
+<div id="footer-left"><!-- blank --></div>
+<div id="footer-right">
+<p><a href="http://wordpress.com/" rel="generator">Get a cool blog at WordPress.com</a>. Theme: Vermilion.</p>
+<div id="footer-meta">
+</div><!-- end id="footer-meta" -->
+</div><!-- end id="footer-right" -->
+</div><!-- end id="footer-wrap" -->
+</div><!-- end id="footer" -->
+</div><!-- end id="page" -->
+<?php wp_footer(); ?>
+</body>
+</html>
