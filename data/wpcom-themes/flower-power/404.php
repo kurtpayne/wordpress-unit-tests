9a823075dff7c4326cc0eca1df894dd228821c13
Kurt Payne <kpayne@gmail.com>
2013-03-19 21:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/flower-power/404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<!-- content ................................. -->
+<div id="content">
+
+	<h2><?php _e('Error 404 &ndash; File not Found', 'flower-power'); ?></h2>
+	
+	<p><?php _e('Sorry, but the page you were looking for could not be found.', 'flower-power'); ?></p>
+	
+</div> <!-- /content -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
