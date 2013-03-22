b2207ef9091790dc5af6dea2a9e021ebad2a2fe5
Kurt Payne <kpayne@gmail.com>
2013-03-22 06:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/flower-power/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffff',
+	'text' => '000000',
+	'link' => '1d698f'
+);
+
+$content_width = 340;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
