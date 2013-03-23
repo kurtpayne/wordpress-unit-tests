df3e3cbf4c2f7fa871e74315fdac7d7b85bd99ce
Kurt Payne <kpayne@gmail.com>
2013-03-23 14:05:41 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fjords/searchform.php
@@ -0,0 +1,6 @@
+<form method="get" action="/">
+<p>
+<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" size="18" name="s" id="s" />
+<input type="submit" value="<?php _e('Search'); ?>" />
+</p>
+</form>
