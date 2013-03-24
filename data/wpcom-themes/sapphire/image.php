1f698c9cceeb6198d300c7cff69585029de9a5f2
Kurt Payne <kpayne@gmail.com>
2013-03-24 05:05:40 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sapphire/image.php
@@ -0,0 +1,57 @@
+<?php get_header(); ?>
+
+	<div id="content" class="widecolumn">
+				
+  <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div <?php post_class(); ?>>
+			<h2 id="post-<?php the_ID(); ?>"><a href="<?php echo get_permalink($post->post_parent); ?>" rev="attachment"><?php echo get_the_title($post->post_parent); ?></a> &raquo; <?php the_title(); ?></h2>
+
+			<div class="entrytext">
+				<p class="attachment"><a href="<?php echo wp_get_attachment_url($post->ID); ?>"><?php echo wp_get_attachment_image( $post->ID, 'auto' ); ?></a></p>
+				<div class="caption"><?php if ( !empty($post->post_excerpt) ) the_excerpt(); ?></div>
+				<div class="image-description"><?php if ( !empty($post->post_content) ) the_content(); ?></div>
+
+				<div class="navigation">
+					<div class="alignleft"><?php previous_image_link() ?></div>
+					<div class="alignright"><?php next_image_link() ?></div>
+				</div>
+
+				<p class="postmetadata alt">
+					<small>
+						You can subscribe via <?php comments_rss_link('RSS 2.0'); ?> feed to this post's comments. 
+												
+						<?php if (('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+							// Both Comments and Pings are open ?>
+							You can <a href="#respond">comment below</a>, or <a href="<?php trackback_url(display); ?>">link to this permanent URL</a> from your own site. 
+						
+						<?php } elseif (!('open' == $post-> comment_status) && ('open' == $post->ping_status)) {
+							// Only Pings are Open ?>
+							Responses are currently closed, but you can <a href="<?php trackback_url(display); ?> ">trackback</a> from your own site.
+						
+						<?php } elseif (('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+							// Comments are open, Pings are not ?>
+							You can skip to the end and leave a response. Pinging is currently not allowed.
+			
+						<?php } elseif (!('open' == $post-> comment_status) && !('open' == $post->ping_status)) {
+							// Neither Comments, nor Pings are open ?>
+							Both comments and pings are currently closed.			
+						
+						<?php } edit_post_link('Edit this entry.','',''); ?>
+						
+					</small>
+				</p>
+	
+			</div>
+		</div>
+		
+	<?php comments_template(); ?>
+	
+	<?php endwhile; else: ?>
+	
+		<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+	
+<?php endif; ?>
+	
+	</div>
+
+<?php get_footer(); ?>
