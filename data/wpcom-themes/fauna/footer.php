39383084a7a6ef7082fc4d778f6e8262ae12886f
Kurt Payne <kpayne@gmail.com>
2013-03-28 14:05:50 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fauna/footer.php
@@ -0,0 +1,13 @@
+</div><!-- //wrapper -->
+
+<div id="footer">
+	<a href="<?php bloginfo('rss2_url'); ?>" title="<?php _e('Syndicate Entries using RSS 2.0') ?>"><?php _e('Entries XML') ?></a>
+	 | <a href="<?php bloginfo('comments_rss2_url'); ?>" title="<?php _e('Syndicate Comments using RSS 2.0') ?>"><?php _e('Comments XML') ?></a>
+	 | <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: <a href="http://www.noscope.com/fauna/" title="Highly adaptive open-source draping for WordPress sites" rel="designer">Fauna</a>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
