1995993452aee8d5868b68002aefd2a213c0e3a5
Kurt Payne <kpayne@gmail.com>
2013-03-30 03:05:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/almost-spring/footer.php
@@ -0,0 +1,18 @@
+<div id="footer">
+<p>
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+&#183;
+<a href="http://beccary.com" rel="designer"><?php _e('Design by Beccary','almost-spring'); ?></a>
+&#183;
+<a href="http://validator.w3.org/check/referer" title="<?php _e('This page validates as XHTML 1.0 Transitional','almost-spring'); ?>"><?php _e('<abbr title="eXtensible HyperText Markup Language">XHTML</abbr>','almost-spring'); ?></a>
+&#183;
+<a href="http://jigsaw.w3.org/css-validator/check/referer" title="<?php _e('This page validates as CSS','almost-spring'); ?>"><?php _e('<abbr title="Cascading Style Sheets">CSS</abbr>','almost-spring'); ?></a>
+</p>
+</div>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
