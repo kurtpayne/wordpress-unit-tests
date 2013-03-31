a2ca9deabf6fa7a276db63cdf1ea4bc4848ce470
Kurt Payne <kpayne@gmail.com>
2013-03-31 02:05:48 -0700

    Updating from SVN

---
+++ data/wpcom-themes/green-marinee/searchform.php
@@ -0,0 +1,9 @@
+                        <p class="searchinfo"><?php _e('search site archives','greenmarinee'); ?></p>
+                        <div id="search">
+                                <div id="search_area">
+                                        <form id="searchform" method="get" action="<?php bloginfo('home'); ?>/">
+                                                <input class="searchfield" type="text" name="s" id="s" value="" title="<?php _e('Enter keyword to search','greenmarinee'); ?>" />
+                                                <input class="submit" type="submit" name="submit" value="" title="<?php _e('Click to search archives','greenmarinee'); ?>" />
+                                        </form>
+                                </div>
+                        </div>
