ee72a67c3d0a995134e417185480e1a9ef5cb336
Kurt Payne <kpayne@gmail.com>
2013-04-04 09:05:45 -0700

    Updating from SVN

---
+++ data/wpcom-themes/depo-masthead/searchform.php
@@ -0,0 +1,7 @@
+
+
+<form id="searchform" name="searchform" method="get" action="/?s=">
+<input type="text" name="s" value=""  />
+<input type="submit" id="searchsubmit" style="display: none;" value="<?php _e('Search', 'depo-masthead'); ?>" />
+</form>
+
