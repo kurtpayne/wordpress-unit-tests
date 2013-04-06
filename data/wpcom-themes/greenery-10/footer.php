703ef368e15cfbe2ec919ade1c4aff80d446b384
root <root@jenkins.(none)>
2013-04-06 01:15:47 -0700

    Updating from SVN

---
+++ data/wpcom-themes/greenery-10/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+<p>
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: Greenery by <a href="http://www.ilemoned.com/">iLemoned</a>.
+</p>
+</div>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
