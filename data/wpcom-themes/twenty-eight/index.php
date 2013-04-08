99f43c37688fed0fed6e0a72ba38d927ea90540d
root <root@jenkins.(none)>
2013-04-08 07:15:44 -0700

    Updating from SVN

---
+++ data/wpcom-themes/twenty-eight/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="content">
+	<div class="primary">
+		<?php include (TEMPLATEPATH . '/theloop.php'); ?>
+	</div> <!-- close primary -->
+	<?php get_sidebar(); ?>
+</div> <!-- close content -->
+	<?php get_footer(); ?>
