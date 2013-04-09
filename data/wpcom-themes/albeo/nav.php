bff4baab3d76eae3725d68340d229895fed04b1a
root <root@jenkins.(none)>
2013-04-09 12:15:35 -0700

    Updating from SVN

--- data/wpcom-themes/albeo/nav.php
+++
@@ -1,4 +0,0 @@
-<div class="nav">
- 	<div class="left"><?php next_posts_link(__('Older Entries', 'albeo')) ?></div>
- 	<div class="right"><?php previous_posts_link(__('Newer Entries', 'albeo')) ?></div>
-</div>
