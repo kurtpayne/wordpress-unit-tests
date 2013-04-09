d9491da52124e0aefd8242484df1ca0cf51d75c4
root <root@jenkins.(none)>
2013-04-09 14:15:47 -0700

    Updating from SVN

---
+++ data/wpcom-themes/pressrow/footer.php
@@ -0,0 +1,9 @@
+		<div id="footer">
+<p><a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com'); ?></a> | Theme: Pressrow by <a href="http://www.pearsonified.com/">Chris Pearson</a>.</p>
+		</div>
+	
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
