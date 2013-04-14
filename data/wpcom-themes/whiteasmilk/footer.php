d1d55522222f6af99abb06f72815f78b585a9bb5
root <root@jenkins.(none)>
2013-04-14 12:16:10 -0700

    Updating from SVN

---
+++ data/wpcom-themes/whiteasmilk/footer.php
@@ -0,0 +1,14 @@
+
+<div id="footer">
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> <?php _e('Theme:','whiteasmilk'); ?> <a href="http://www.azeemazeez.com/stuff/themes/" title="White as Milk theme for WordPress" rel="designer">White as Milk</a>.<br />
+
+<a href="<?php bloginfo('rss2_url'); ?>"><?php _e('Entries','whiteasmilk'); ?></a> <?php _e('and','whiteasmilk'); ?> <a href="<?php bloginfo('comments_rss2_url'); ?>"><?php _e('comments','whiteasmilk'); ?></a><?php _e(' feeds.','whiteasmilk'); ?> 
+<?php _e('Valid','whiteasmilk'); ?> <a href="http://validator.w3.org/check/referer">XHTML</a> <?php _e('and','whiteasmilk'); ?> <a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>.
+
+</div>
+</div>
+
+		<?php do_action('wp_footer'); ?>
+
+</body>
+</html>
