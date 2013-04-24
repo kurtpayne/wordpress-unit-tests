1cabe9aa446784b1be9a9311adbebf4aea795ff9
root <root@jenkins.(none)>
2013-04-24 07:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/k2/footer.php
@@ -0,0 +1,15 @@
+	<div class="clear"></div>
+
+</div> <!-- Close Page -->
+
+<hr />
+
+<p id="footer"><small>
+	<?php printf(__('Theme: %s by %s.', 'k2_domain'), 'k2', '<a href="http://getk2.com/" rel="designer">k2 team</a>'); ?> <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+	<br /> 
+	<?php printf(__('<a href="%1$s">RSS Entries</a> and <a href="%2$s">RSS Comments</a>','k2_domain'), get_bloginfo('rss2_url'), get_bloginfo('comments_rss2_url')) ?>
+</small></p>
+
+	<?php do_action('wp_footer'); ?>
+</body>
+</html> 
