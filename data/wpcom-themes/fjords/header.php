5909b63f5ad6d457d44c797e4f0192b78cba8386
root <root@jenkins.(none)>
2013-04-24 21:15:42 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fjords/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
+
+
+	<style type="text/css" media="screen">
+		@import url( <?php bloginfo('stylesheet_url'); ?> );
+	</style>
+
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php 
+	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+	wp_head(); 
+	?>
+
+</head>
+
+<body>
+
+<div id="wrapper">
+
+	<div id="content">
+
+<div id="hode">
+<h4><a href="<?php echo get_settings('home'); ?>"><?php bloginfo('title'); ?></a></h4>
+<span><?php bloginfo('description'); ?></span>
+</div>
