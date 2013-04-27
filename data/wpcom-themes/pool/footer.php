8640f3352d5fd1833fff03d8b6d06ac53a1bd3fb
root <root@jenkins.(none)>
2013-04-27 15:15:34 -0700

    Updating from SVN

---
+++ data/wpcom-themes/pool/footer.php
@@ -0,0 +1,17 @@
+<!-- begin footer -->
+
+<?php get_sidebar(); ?>
+
+<p id="credits">
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: Pool by <a href="http://www.lamateporunyogur.net/" rel="designer">Borja Fernandez</a>.<br />
+
+<a href="<?php bloginfo('rss2_url'); ?>">Entries</a> and <a href="<?php bloginfo('comments_rss2_url'); ?>">comments</a> feeds. 
+
+<!-- <?php echo $wpdb->num_queries; ?> queries. <?php timer_stop(1); ?> seconds. -->
+</p>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+</body>
+</html>
