c0166df9945ba3419b6cb4986528ff8472e8fb98
root <root@jenkins.(none)>
2013-04-29 15:15:35 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sandbox-10/404.php
@@ -0,0 +1,23 @@
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content">
+
+			<div id="post-0" class="post error404">
+				<h2 class="entry-title"><?php _e('Not Found', 'sandbox') ?></h2>
+				<div class="entry-content">
+					<p><?php _e('Apologies, but we were unable to find what you were looking for. Perhaps  searching will help.', 'sandbox') ?></p>
+				</div>
+				<form id="error404-searchform" method="get" action="<?php bloginfo('home') ?>">
+					<div>
+						<input id="error404-s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true) ?>" size="40" />
+						<input id="error404-searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Find', 'sandbox') ?>" />
+					</div>
+				</form>
+			</div><!-- .post -->
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+
+<?php get_sidebar() ?>
+<?php get_footer() ?>
