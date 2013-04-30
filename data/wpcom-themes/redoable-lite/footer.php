64c3a5d665145abf1c04277797963efffe236e8c
root <root@jenkins.(none)>
2013-04-30 00:15:28 -0700

    Updating from SVN

---
+++ data/wpcom-themes/redoable-lite/footer.php
@@ -0,0 +1,27 @@
+	<div class="clear"></div>
+</div> <!-- Close Page -->
+
+<hr />
+
+<div id="footer"><!-- UNSLEEPABLE FOOTER -->
+	<div id="footer_content">
+		<div id="footer_middle">
+     		<div id="footer_images">
+
+     		</div>
+   		
+		<!-- spacer -->
+		<div id="copyright"><p>&nbsp;</p></div>
+
+			<small>
+				<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: Redoable Lite by <a href="http://www.deanjrobinson.com/" rel="designer">Dean J Robinson</a>
+   			</small>
+	
+		</div>
+	</div>
+</div><!-- END UNSLEEPABLE FOOTER -->
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html> 
