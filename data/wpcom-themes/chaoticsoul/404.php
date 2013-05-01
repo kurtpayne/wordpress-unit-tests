3b6adbd400975204ba42d74672d9f8aed2288227
root <root@jenkins.(none)>
2013-04-30 20:15:31 -0700

    Updating from SVN

---
+++ data/wpcom-themes/chaoticsoul/404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+	<div id="content" class="widecolumn">
+
+	<h2 class="title"><?php _e('Problem?', 'chaoticsoul'); ?></h2>
+		<p><?php _e("Looks like you have a problem here sir/madam. You sure you have the right place? Maybe you got a little lost? Maybe you're looking for something you're not supposed to find? Either way, just go search for it, it can't hurt.", 'chaoticsoul'); ?></p>
+
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
