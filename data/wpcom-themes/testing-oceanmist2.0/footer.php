f69b50695777a77e15600c3da52b38a184a3b1ed
root <root@jenkins.(none)>
2013-05-06 22:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/testing-oceanmist2.0/footer.php
@@ -0,0 +1,12 @@
+		</div>
+		<div id="footer">
+		  <p>
+			<?php om_footer(); ?>
+			<br /><?php bloginfo('name'); ?> is powered by <a href="http://wordpress.org/">WordPress</a> | Photo by <a href="http://www.sxc.hu/profile/rexfarucao">rexfarucao</a></p>
+		</div>
+	</div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
+
