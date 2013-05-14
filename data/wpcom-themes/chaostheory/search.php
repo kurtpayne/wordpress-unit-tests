df42386518f6222e325656365c1da8b36c9b46a2
root <root@jenkins.(none)>
2013-05-14 16:15:31 -0700

    Updating from SVN

---
+++ data/wpcom-themes/chaostheory/search.php
@@ -0,0 +1,63 @@
+<?php get_header() ?>
+
+	<div id="container">
+		<div id="content" class="hfeed">
+
+<?php if (have_posts()) : ?>
+
+		<h2 class="page-title"><?php _e('Search Results for:', 'sandbox') ?> <?php echo wp_specialchars(stripslashes($_GET['s']), true); ?></h2>
+
+			<div id="nav-above" class="navigation">
+				<div class="nav-previous"><?php next_posts_link(__('&laquo; Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts &raquo;', 'sandbox')) ?></div>
+			</div>
+
+<?php while (have_posts()) : the_post(); ?>
+
+			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
+				<div class="entry-meta">
+					<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf(__('Permalink to %s', 'sandbox'), wp_specialchars(get_the_title(), 1)) ?>" rel="bookmark"><?php the_title() ?></a></h2>
+					<ul>
+						<li class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO'); ?>"><?php unset($previousday); printf(__('%1$s &#8211; %2$s', 'sandbox'), the_date('', '', '', false), get_the_time()) ?></abbr></li>
+						<!-- <li class="entry-author author vcard"><?php printf(__('By %s', 'sandbox'), '<a class="url fn" href="'.get_author_link(false, $authordata->ID, $authordata->user_nicename).'" title="View all posts by ' . $authordata->display_name . '">'.get_the_author().'</a>') ?></li> -->
+						<li class="entry-category"><?php printf(__('Posted in %s', 'sandbox'), get_the_category_list(', ')) ?></li>
+						<?php the_tags('<li class="entry-tags">'.__('Tagged').' ', ", ", "</li>\n") ?>
+<?php edit_post_link(__('Edit', 'sandbox'), "\t\t\t\t\t<li class='entry-editlink'>", "</li>"); ?>
+						<li class="entry-commentlink"><?php comments_popup_link(__('Leave a Comment', 'sandbox'), __('Comments (1)', 'sandbox'), __('Comments (%)', 'sandbox')) ?></li>
+					</ul>
+				</div>
+				<div class="entry-content">
+<?php the_content('<span class="more-link">'.__('Read More &raquo;', 'sandbox').'</span>'); ?>
+
+				</div>
+			</div><!-- .post -->
+
+<?php endwhile; ?>
+
+			<div id="nav-below" class="navigation">
+				<div class="nav-previous"><?php next_posts_link(__('&laquo; Older posts', 'sandbox')) ?></div>
+				<div class="nav-next"><?php previous_posts_link(__('Newer posts &raquo;', 'sandbox')) ?></div>
+			</div>
+
+<?php else : ?>
+
+			<div id="post-0" class="post">
+				<h2 class="entry-title"><?php _e('Nothing Found', 'sandbox') ?></h2>
+				<div class="entry-content">
+					<p class="search-error"><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'sandbox') ?></p>
+				</div>
+			</div>
+			<form id="searchform" method="get" action="<?php bloginfo('home') ?>">
+				<div>
+					<input id="s" name="s" type="text" value="<?php echo wp_specialchars(stripslashes($_GET['s']), true); ?>" tabindex="1" size="40" />
+					<input id="searchsubmit" name="searchsubmit" type="submit" value="<?php _e('Search &raquo;', 'sandbox') ?>" tabindex="2" />
+				</div>
+			</form>
+
+<?php endif; ?>
+
+		</div><!-- #content .hfeed -->
+	</div><!-- #container -->
+
+<?php get_sidebar() ?>
+<?php get_footer() ?>
