8803adf9c62ff83a1ea9c2c9c595e62ec89ec147
root <root@jenkins.(none)>
2013-05-17 21:15:34 -0700

    Updating from SVN

---
+++ data/wpcom-themes/vermilion-christmas/functions.php
@@ -0,0 +1,13 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffff',
+	'text' => '343434',
+	'link' => '990000'
+);
+
+$content_width = 540; // actually 543
+
+if ( function_exists('register_sidebar') )
+    register_sidebar();
+?>
