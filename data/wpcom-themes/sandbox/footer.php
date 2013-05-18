e5395e8ebd5d481e34b13159b907ffcc518998c4
root <root@jenkins.(none)>
2013-05-18 04:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sandbox/footer.php
@@ -0,0 +1,12 @@
+	<div id="footer">
+		<span id="generator-link"><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></span>
+		<span class="metasep">|</span>
+		<span id="theme-link"><a href="http://www.plaintxt.org/themes/sandbox/" title="Sandbox theme for WordPress (0.6.1)" rel="designer">Sandbox</a></span>
+	</div><!-- #footer -->
+
+</div><!-- #wrapper -->
+
+<?php wp_footer() ?>
+
+</body>
+</html>
