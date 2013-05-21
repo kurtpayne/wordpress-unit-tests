72bb81693e3f76d172db7a9a7fda3d3808ef2bf5
root <root@jenkins.(none)>
2013-05-21 12:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/ocadia/footer.php
@@ -0,0 +1,14 @@
+<div id="footer">
+<p>
+<a href="http://beccary.com" rel="designer"><?php _e('Design by Beccary'); ?></a>
+&#183;
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+</p>
+</div>
+
+</div>
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+</html>	
