aa9f48a46640748f6c1b679eaece4b98acaba1ba
root <root@jenkins.(none)>
2013-05-21 21:15:34 -0700

    Updating from SVN

---
+++ data/wpcom-themes/ocean-mist/footer.php
@@ -0,0 +1,9 @@
+
+  <div id="footer">
+    <div id="footer-top"></div>
+	<p><a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com<'); ?>/a> | Theme: Ocean Mist by <a href="http://www.edmerritt.com/" rel="designer">Ed Merritt</a></p>
+  </div>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
