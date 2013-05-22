607d7bc9b18399cce7c75434d0e5b186c8be7888
root <root@jenkins.(none)>
2013-05-21 20:15:34 -0700

    Updating from SVN

---
+++ data/wpcom-themes/unsleepable/footer.php
@@ -0,0 +1,17 @@
+<div style="clear:both;"></div>
+</div>
+<!-- Close Page -->
+
+<div id="footer"><br /><hr />
+
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> &bull; Theme: Unsleepable by <a href="http://www.openswitch.org">Ben Gray</a>
+
+<?php do_action('wp_footer'); ?>
+
+</div>
+
+<?php /* Try. to understand */ ?>
+
+
+</body>
+</html> 
