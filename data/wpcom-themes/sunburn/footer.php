0ee792938fa3691d22cb2eb28c97ac7e41f45ff8
root <root@jenkins.(none)>
2013-05-22 01:15:33 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sunburn/footer.php
@@ -0,0 +1,18 @@
+		</div>
+
+		<div id="bar"></div>
+		
+		<div id="footer_bg">
+			<div id="footer">
+				<p>
+					<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>. Theme: Sunburn by <a href="http://jimwhimpey.com/" rel="designer">Jim Whimpey</a>.
+				</p>
+			</div>
+		</div>
+
+	</div>
+	
+	<?php wp_footer(); ?>
+
+</body>
+</html>
