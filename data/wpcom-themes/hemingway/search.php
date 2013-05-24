2fc0e9278933c470fa8821977c313b6868b0b46d
root <root@jenkins.(none)>
2013-05-24 09:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/hemingway/search.php
@@ -0,0 +1,56 @@
+<?php get_header(); ?>
+
+	<div id="primary" class="single-post">
+	<div class="inside">
+		<div class="primary">
+
+	<?php if (have_posts()) : ?>
+
+		<h1>Search Results</h1>
+		
+		<ul class="dates">
+		 	<?php while (have_posts()) : the_post(); ?>
+			<li>
+				<span class="date"><?php the_time(get_option('date_format')) ?></span>
+				<a href="<?php the_permalink() ?>"><?php the_title(); ?></a> 
+				 posted in 
+				<?php the_category(', ') ?>  
+				<?php if (is_callable('the_tags')) the_tags('tagged ', ', '); ?>
+			</li>
+			<?php $results++; ?>
+			<?php endwhile; ?>
+		</ul>
+		
+		<div class="navigation">
+			<div class="left"><?php next_posts_link('&laquo; Previous Entries') ?></div>
+			<div class="right"><?php previous_posts_link('Next Entries &raquo;') ?></div>
+		</div>
+	
+	<?php else : ?>
+
+		<h1>No posts found. Try a different search?</h1>
+
+	<?php endif; ?>
+		
+	</div>
+	
+	<div class="secondary">
+		<h2>Search</h2>
+		<div class="featured">
+			<p>You searched for &ldquo;<?php the_search_query(); ?>&rdquo; at <?php bloginfo('name'); ?>. There were
+			<?php
+				if (!$results) echo "no results, better luck next time.";
+				elseif (1 == $results) echo "one result found. It must be your lucky day.";
+				else echo $results . " results found.";
+			?>
+			</p>
+			
+		</div>
+	</div>
+	<div class="clear"></div>
+	</div>
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
