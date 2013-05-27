2d9d2c70ba74c4d13bcc397ae4ae58a17bad7360
root <root@jenkins.(none)>
2013-05-27 05:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/connections/footer.php
@@ -0,0 +1,3 @@
+<p id="footer"><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> &#8212; Theme: Connections by <a href="http://www.vanillamist.com" rel="designer">www.vanillamist.com</a></p>
+
+<?php do_action('wp_footer'); ?>
