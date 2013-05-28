4884a094ac6fb04e260c2ff7d5aadf3ce0a2bc23
root <root@jenkins.(none)>
2013-05-28 06:15:33 -0700

    Updating from SVN

---
+++ data/wpcom-themes/andreas09/footer.php
@@ -0,0 +1,13 @@
+</div>
+<div class="clearingdiv">&nbsp;</div>
+</div>
+
+<div id="footer">
+<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a> | Theme: Andreas09 by <a href="http://andreasviklund.com" rel="designer">Andreas Viklund</a>.
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+
+</html>
