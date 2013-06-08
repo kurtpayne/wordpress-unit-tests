d349ab7a5f33cd7d00aa5dad605a8ba2ebd7defc
root <root@jenkins.(none)>
2013-06-08 06:15:03 -0700

    Updating from SVN

--- data/wpcom-themes/depo-masthead/searchform.php
+++
@@ -1,7 +0,0 @@
-
-
-<form id="searchform" name="searchform" method="get" action="/?s=">
-<input type="text" name="s" value=""  />
-<input type="submit" id="searchsubmit" style="display: none;" value="<?php _e('Search', 'depo-masthead'); ?>" />
-</form>
-
