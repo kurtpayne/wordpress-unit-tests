3c17ba58e73446c0cba18151a4bc5f3a442bfef5
root <root@jenkins.(none)>
2013-06-08 19:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/thirteen/searchform.php
@@ -0,0 +1,6 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<p>
+<input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+<input type="submit" id="searchsubmit" class="submit" value="Search" />
+</p>
+</form>
