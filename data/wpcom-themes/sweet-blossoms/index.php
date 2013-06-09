e7d60a56e4b77cd84a70cc09082d9377d72d0fdb
root <root@jenkins.(none)>
2013-06-09 07:15:28 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sweet-blossoms/index.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+<!-- content ................................. -->
+<div id="main">
+
+<?php if (have_posts()) : ?>
+
+<?php while (have_posts()) : the_post(); ?>
+
+	<div <?php post_class('main'); ?>>
+
+		<h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
+
+		<?php ($post->post_excerpt != "")? the_excerpt() : the_content(); ?>
+
+		<p class="info"><?php if ($post->post_excerpt != "") { ?><a href="<?php the_permalink() ?>" class="more">Continue Reading</a><?php } ?>
+   		<?php comments_popup_link('Add comment', '1 comment', '% comments', 'commentlink', ''); ?>
+   		<em class="date"><?php the_time(get_option('date_format')) ?><!-- at <?php the_time()  ?>--></em>
+   		<em class="author"><?php the_author(); ?></em>
+   		<?php edit_post_link('Edit','<span class="editlink">','</span>'); ?>
+		<br /> <?php the_tags('Tags: ', ', ', '<br />'); ?>
+   		</p>
+
+		<div style="padding:20px 0px 0px 0px;"></div>
+		
+		<img src="<?php bloginfo('stylesheet_directory'); ?>/images/divider.gif" alt="" />
+		
+		<div style="padding:20px 0px 0px 0px;"></div>
+		
+	</div>
+
+<?php endwhile; ?>
+
+	<table width="100%" cellpadding="0" cellspacing="0">
+		<tr>
+			<td width="120" align="left"><?php next_posts_link('Previous Posts') ?></td>
+			<td width="60"></td>
+			<td width="120" align="right"><?php previous_posts_link('Next Posts') ?></td>
+</tr>
+	</table>
+
+<?php else : ?>
+
+	<h2>Not Found</h2>
+	<p>Sorry, but you are looking for something that isn't here.</p>
+
+<?php endif; ?>
+
+</div> 
+
+<!-- /content -->
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
