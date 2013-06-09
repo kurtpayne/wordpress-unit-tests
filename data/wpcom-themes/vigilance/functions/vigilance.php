dcc6928ce971a6e25fb780e72233dadd5e5639ca
root <root@jenkins.(none)>
2013-06-08 20:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/vigilance/functions/vigilance.php
@@ -0,0 +1,3 @@
+<?php $copyright_name = get_option('V_copyright_name'); ?>
+    <p class="right"><a href="http://wordpress.com/" rel="generator"><?php printf(__('Get a free blog at %s', 'vigilance'), 'WordPress.com'); ?></a></p>
+		<p><?php printf(__('Theme: %s by %s', 'vigilance'), 'Vigilance', '<a href="http://www.jestro.com/" rel="designer">Jestro</a>'); ?></p>
