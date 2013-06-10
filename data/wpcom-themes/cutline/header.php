59db5394a127d29159d027af74d6217c35c44fd0
root <root@jenkins.(none)>
2013-06-10 04:15:39 -0700

    Updating from SVN

---
+++ data/wpcom-themes/cutline/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<?php 
+if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+wp_head(); ?>
+</head>
+<body>
+
+<div id="container">
+
+	<div id="masthead"><h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1></div>
+
+	<ul id="nav">
+		<li><a <?php if (is_home()) echo('class="current" '); ?>href="<?php bloginfo('url'); ?>"><?php _e('Front Page','cutline'); ?></a></li>
+		<?php wp_list_pages('title_li=&sort_column=menu_order&depth=1'); ?>
+	</ul>
+	
+	<div id="header_img">
+		<img src="<?php header_image() ?>" width="770" height="140" alt="" />
+	</div>
