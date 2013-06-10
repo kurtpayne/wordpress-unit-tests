ceb7ca3afc42497939ec9d5ef3829378e392f9c6
root <root@jenkins.(none)>
2013-06-10 10:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/blix/footer.php
@@ -0,0 +1,16 @@
+
+<hr class="low" />
+
+<!-- footer ................................. -->
+<div id="footer">
+
+	<p>Theme: Blix by <a href="http://www.kingcosmonaut.de/" rel="designer">Sebastian Schmieg</a> . <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></p>
+	<?php do_action('wp_footer'); ?>
+
+</div> <!-- /footer -->
+
+</div> <!-- /container -->
+
+</body>
+
+</html>
