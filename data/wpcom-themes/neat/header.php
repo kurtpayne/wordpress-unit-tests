e46f9964a3ca76e2bb97d827d3d646d90c560375
root <root@jenkins.(none)>
2013-06-11 21:15:31 -0700

    Updating from SVN

---
+++ data/wpcom-themes/neat/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+
+<head profile="http://gmpg.org/xfn/11">
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+<title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
+	
+
+	<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>?1" type="text/css" media="screen" />
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<?php
+	if ( is_singular() ) wp_enqueue_script( 'comment-reply' );
+	wp_head(); 
+	?>
+</head>
+<body>
+<br /><br /><br />
+<div id="page">
+
+<div id="header" onclick="location.href='<?php bloginfo('url'); ?>';" style="cursor: pointer;">
+    <h1><a href="<?php echo get_settings('home'); ?>"><?php bloginfo('name'); ?></a></h1>
+    <div class="description"><?php bloginfo('description'); ?></div>
+</div>
