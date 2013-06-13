b0f2e9b3b7291134d9f0f9522e84211304ba8cb9
root <root@jenkins.(none)>
2013-06-13 07:15:03 -0700

    Updating from SVN

--- data/wpcom-themes/pressrow/searchform.php
+++
@@ -1,4 +0,0 @@
-<form method="get" id="search_form" action="<?php bloginfo('home'); ?>/">
-	<p style="margin-bottom: 5px;"><input type="text" class="text_input" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" /></p>
-	<p style="margin-bottom: 0;"><input type="submit" id="searchsubmit" value="<?php echo attribute_escape(__('Search')); ?>" /></p>
-</form>
