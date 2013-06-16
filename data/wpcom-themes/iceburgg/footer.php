df6481404bda070fd680541bccb6a689877f59d5
root <root@jenkins.(none)>
2013-06-16 05:15:31 -0700

    Updating from SVN

---
+++ data/wpcom-themes/iceburgg/footer.php
@@ -0,0 +1,9 @@
+<div style="clear: both"></div>
+<div id="prefooter"></div>
+<div id="footer"> 
+	<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>. Theme: Iceburgg by <a href="http://www.devlounge.net" rel="designer">Devlounge</a>.
+<?php wp_footer(); ?>
+</div>
+<div style="clear: both"></div>
+
+</body></html>
