5037a8e622e7f8cb139069fd886b0fc44b9cea2c
root <root@jenkins.(none)>
2013-06-17 17:15:32 -0700

    Updating from SVN

---
+++ data/wpcom-themes/monotone/footer.php
@@ -0,0 +1,18 @@
+			<div id="footer">
+			<?php get_sidebar(); ?>
+			<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+			<p class="info">
+					<a href="<?php bloginfo('rss2_url'); ?>"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/rss.png" alt="RSS" /></a>
+					<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+				<!-- <?php echo get_num_queries(); ?> queries. <?php timer_stop(1); ?> seconds. -->
+			</p>
+			<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+			</div>
+	
+		</div>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
