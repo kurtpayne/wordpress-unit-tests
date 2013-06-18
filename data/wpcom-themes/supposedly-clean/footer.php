d4f3b80a444e7ce823cd2be5ed7e6a2999066585
root <root@jenkins.(none)>
2013-06-18 01:15:28 -0700

    Updating from SVN

---
+++ data/wpcom-themes/supposedly-clean/footer.php
@@ -0,0 +1,9 @@
+</div><!--close matt-->
+	<div id="footer"><p>Theme: Supposedly Clean by <a href="http://www.alvinwoon.com/blog" rel="designer">Alvin Woon</a>. <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></p>  </div>
+
+	</div><!--close container-->
+<?php wp_footer(); ?>
+</body>
+</html>
+
+
