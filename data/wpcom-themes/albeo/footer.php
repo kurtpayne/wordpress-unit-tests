fde33f17919df6a4e0d36456ec3e461b6cb5a654
root <root@jenkins.(none)>
2013-06-19 18:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/albeo/footer.php
@@ -0,0 +1,26 @@
+</div>
+<div class="sl-b"></div>
+</div>
+<!-- End Side Left -->
+
+<!-- Start Sidebar -->
+<?php if(!is_attachment()) get_sidebar(); ?>
+<!-- End Sidebar -->
+
+</div>
+<!-- End Con -->
+
+<!-- Start Footer -->
+<div class="footer">
+ <p><a href="http://wordpress.com/" rel="generator"><?php _e('Get a free blog at WordPress.com', 'albeo'); ?></a></p>
+ <p class="theme"><?php printf( __('%s theme by %s', 'albeo'), 'Albeo', '<a href="http://designdisease.com/" rel="designer">Design Disease</a>' ); ?></p>
+</div>
+<!-- End Footer -->
+
+
+</div>
+<!-- End BG -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
