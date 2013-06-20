539c0829b06e06718a1aa2b70110f6a86094daf5
root <root@jenkins.(none)>
2013-06-20 15:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/andreas04/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'e6e6e6',
+	'text' => '000000',
+	'link' => '226699',
+	'border' => 'e6e6e6',
+	'url' => '226699'
+	);
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(2);
+
+?>
