b26bb9a892f0533a8507ffdbbdfc428b9ecb3ec3
root <root@jenkins.(none)>
2013-06-24 03:15:29 -0700

    Updating from SVN

---
+++ data/wpcom-themes/grid-focus/navigation.strip.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ *	@package WordPress
+ *	@subpackage Grid_Focus
+ */
+?>
+<div class="navStripWrapper">
+	
+	<ul class="nav fix">
+		<li><a href="<?php echo get_settings('home'); ?>/" title="Return to the the frontpage">Frontpage<br /><span>Return home</span></a></li>
+		<li><a id="triggerCatID" href="#" title="Show categories">Browse<br /><span>By topic</span></a></li>
+		<li class="last"><a href="<?php bloginfo('rss2_url'); ?>" title="Subscribe to the main feed via RSS">Subscribe<br /><span>RSS feed</span></a></li>
+		<li id="searchBar">
+			<?php include (TEMPLATEPATH . '/searchform.php'); ?>
+		</li>
+	</ul>
+
+	<div id="headerStrip" class="toggleCategories fix" style="display: none;"> 
+		<ul class="fix">
+		<?php wp_list_cats('sort_column=name&optioncount=0&exclude=10, 15'); ?>
+		</ul>
+	</div>
+	
+</div>
