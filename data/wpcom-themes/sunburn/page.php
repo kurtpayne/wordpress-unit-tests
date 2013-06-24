eef8df19e35546924a41d5b35ffb4284cf90f302
root <root@jenkins.(none)>
2013-06-24 04:15:27 -0700

    Updating from SVN

---
+++ data/wpcom-themes/sunburn/page.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+	<div id="content" class="page">
+
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		<div <?php post_class('post'); ?> id="post-<?php the_ID(); ?>">
+		<h2><?php the_title(); ?></h2>
+			<div class="entrytext" id="pagetext">
+				<?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+	
+				<?php link_pages('<p><strong>Pages:</strong> ', '</p>', 'number'); ?>
+	
+			</div>
+		</div>
+	  <?php endwhile; endif; ?>
+	<?php edit_post_link('Edit this entry.', '<p>', '</p>'); ?>
+	<?php comments_template(); ?>
+	</div>
+
+<?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
