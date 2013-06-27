26ab1dbd5d9dc6ed44355173bec9996bf177eca5
root <root@jenkins.(none)>
2013-06-27 15:15:21 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fauna/nav.php
@@ -0,0 +1,5 @@
+<div class="box">
+	<div class="align-left"><?php next_posts_link(__('&laquo; Older Entries')) ?></div>
+	<div class="align-right"><?php previous_posts_link(__('Newer Entries &raquo;')) ?></div>
+	<br class="clear" />
+</div>
