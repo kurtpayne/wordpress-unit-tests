1bac70ae0fbd5d5592d003189d53ba0fb3fdad67
root <root@jenkins.(none)>
2013-06-27 02:15:04 -0700

    Updating from SVN

--- data/wpcom-themes/jentri/footer.php
+++
@@ -1,4 +0,0 @@
-<div id="footer">Theme: Jentri by <a href="http://ifelse.co.uk" rel="designer">Phu</a>. 
-<a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></div>
-
-<?php wp_footer(); ?>
