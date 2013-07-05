ee1b89db6098fa75b649bb2d6d447ba358879793
root <root@jenkins.(none)>
2013-07-04 22:15:27 -0700

    Updating from SVN

---
+++ data/wpcom-themes/regulus/themetoolkit.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Hack Name: WordPress Theme Toolkit
+Plugin URI: http://frenchfragfactory.net/ozh/my-projects/wordpress-theme-toolkit-admin-menu/
+Description: Helps theme authors set up an admin menu. Helps theme users customise the theme.
+Version: 1.0
+Author: Ozh
+Author URI: http://planetOzh.com/
+*/
+
+/************************************************************************************
+ *                             DO NOT MODIFY THIS FILE !
+ ************************************************************************************/
+
+
+
+?>
