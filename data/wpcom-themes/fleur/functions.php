76ba5cea9ab2803e746a54aa4d1521a85748a0b2
root <root@jenkins.(none)>
2013-07-08 06:15:28 -0700

    Updating from SVN

---
+++ data/wpcom-themes/fleur/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'ffffff',
+	'text' => '000000',
+	'link' => 'aa7d39'
+);
+
+$content_width = 500;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
