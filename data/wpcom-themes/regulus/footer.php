b9348dc62f87ecd8bd944e55c8438e4120037d76
root <root@jenkins.(none)>
2013-07-09 07:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/regulus/footer.php
@@ -0,0 +1,10 @@
+
+		<ul id="footer">
+			<li>Theme: Regulus by <a href="http://www.binarymoon.co.uk/" rel="designer">Binary Moon</a></li>
+			<li><a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a></li>
+			<li><a href="#nav" title="Jump to top of page"><?php _e( 'Top' ); ?></a></li>
+		</ul>
+	</div>
+	<?php do_action('wp_footer'); ?>
+</body>
+</html>
