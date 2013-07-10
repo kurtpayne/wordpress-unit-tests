016d3f3df6f19aeee2a99183a5f0953d49f74a48
root <root@jenkins.(none)>
2013-07-09 23:15:28 -0700

    Updating from SVN

---
+++ data/wpcom-themes/chaoticsoul/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<div class="hr">&nbsp;</div>
+<div id="footer">
+	<p><?php printf(__('Get a free blog at %s.', 'chaoticsoul'),'<a href="http://wordpress.com/">WordPress.com</a>'); ?> <?php printf(__('Theme: %s by %s.','chaoticsoul'), 'ChaoticSoul' ,'<a href="http://avalonstar.com" rel="designer">Bryan Veloso</a>'); ?></p>
+</div>
+</div>
+
+<?php wp_footer(); ?>
+</body>
+</html>
