683664bd4e9a9cf6e8d647b6971439908e3adcce
root <root@jenkins.(none)>
2013-07-12 14:16:06 -0700

    Updating from SVN

---
+++ data/wpcom-themes/treba/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+$themecolors = array(
+	'bg' => 'fcfaf4',
+	'text' => '77756b',
+	'link' => '9e9e9e'
+);
+
+$content_width = 420;
+
+if ( function_exists('register_sidebars') )
+	register_sidebars(1);
+
+?>
