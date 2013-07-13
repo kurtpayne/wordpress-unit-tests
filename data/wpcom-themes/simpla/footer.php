5eeb94038b6846e18ba5188c0a99b89f7ae39029
root <root@jenkins.(none)>
2013-07-13 03:15:30 -0700

    Updating from SVN

---
+++ data/wpcom-themes/simpla/footer.php
@@ -0,0 +1,8 @@
+</div>
+<div id="footer">Theme: Simpla by <a href="http://ifelse.co.uk" rel="designer">Phu</a>. <a href="http://wordpress.com/" rel="generator">Get a free blog at WordPress.com</a>
+
+<?php wp_footer(); ?>
+</div>
+
+</body>
+</html>
