6b026b0f5fb3a06f0270a7661f4f3ba9ca0b9c1b
root <root@jenkins.(none)>
2013-07-14 20:15:23 -0700

    Updating from SVN

---
+++ data/wpcom-themes/almost-spring/searchform.php
@@ -0,0 +1,5 @@
+<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
+<p><input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
+<input type="submit" id="searchsubmit" value="<?php _e('Search','almost-spring'); ?>" />
+</p>
+</form>
